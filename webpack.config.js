a4a5fe8eeb2b7b4c493dd6b20c934fe16428f15d
Jami Kousa <jami.kousa@helsinki.fi>
2019-11-01 15:47:42 +0200

    toskaboiler words - mobvita

--- webpack.config.js
+++ webpack.config.js
@@ -75,7 +75,7 @@ module.exports = (env, argv) => {
       }),
       // Skip the part where we would make a html template
       new HtmlWebpackPlugin({
-        title: 'Toska Boilerplate',
+        title: 'Mobvita',
         favicon: path.resolve(__dirname, 'client/assets/favicon-32x32.png'),
         inject: false,
         template: htmlTemplate,
