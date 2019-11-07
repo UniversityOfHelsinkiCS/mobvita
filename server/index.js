da96cd6713d08b78dda728d0c8a52973d71c97f6
Jami Kousa <jami.kousa@helsinki.fi>
2019-11-07 16:20:24 +0200

    initial swagger

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const routes = require('@util/routes')
 const errorMiddleware = require('@middleware/errorMiddleware')
+const swaggerUi = require('swagger-ui-express')
+const YAML = require('yamljs')
+
+const swaggerDocument = YAML.load('./swagger.yaml')
 
 const app = express()
 
 app.use(express.json())
+app.use('/swag', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(routes)
 
 app.use(errorMiddleware)
