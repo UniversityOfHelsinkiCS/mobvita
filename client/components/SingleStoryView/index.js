af385014e00451bfa4ebd240c994a4822314297c
esakemp <esa.kemppainen@helsinki.fi>
2019-11-07 15:06:33 +0200

    jätettä huomiselle

--- client/components/SingleStoryView/index.js
+++ client/components/SingleStoryView/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { Input, Divider, Segment } from 'semantic-ui-react'
 
 import { getStoryAction } from 'Utilities/redux/storiesReducer'
 
@@ -11,12 +12,24 @@ const SingleStoryView = ({ match }) => {
   }, [])
   if (!story) return 'No story (yet?)'
 
+  const wordInputRandomizer = (word) => {
+
+    if (Math.ceil(Math.random() * 4) === 4 && word.bases !== '') {
+      return <Input key={word.ID} defaultValue={word.bases.split('|')[0]}></Input>
+    }
+
+    return word.surface
+  }
+
   return (
     <div style={{ paddingTop: '1em' }}>
       {story.paragraph.map(paragraph => (
-        <p key={paragraph[0].ID}>
-          {paragraph.map(word => word.surface)}
-        </p>
+        <div key={paragraph[0].ID}>
+          <Divider/>
+          <Segment>
+            {paragraph.map(word => wordInputRandomizer(word))}
+          </Segment>
+        </div>
       ))}
     </div>
   )
