dba67ebac9826eda8b070ee3b5b514c3f86b5210
esakemp <esa.kemppainen@helsinki.fi>
2019-11-11 18:17:47 +0200

    tag change

--- webpack.config.js
+++ webpack.config.js
@@ -80,7 +80,7 @@ module.exports = (env, argv) => {
       }),
       // Skip the part where we would make a html template
       new HtmlWebpackPlugin({
-        title: 'Mobvita',
+        title: 'MobVita',
         favicon: path.resolve(__dirname, 'client/assets/favicon-32x32.png'),
         inject: false,
         template: htmlTemplate,
