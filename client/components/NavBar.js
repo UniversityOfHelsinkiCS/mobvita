a69249c18616270bea28348b92c02db52604aa3f
Koskinen Marko J <markokoskinen2037@gmail.com>
2019-11-12 11:40:24 +0200

    created navbar

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,11 +1,37 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React, { useState } from 'react'
+import { Link } from "react-router-dom"
 import { images } from 'Utilities/common'
+import { Menu, Dropdown, Button } from "semantic-ui-react"
 
-export default () => (
-  <div className="navbar">
-    <Link to="/">
-      <img src={images.revitaLogoTransparent} style={{ height: '100%' }} alt="mobvita-logo" />
-    </Link>
-  </div>
-)
+export default NavBar => {
+
+
+  const [active, setActive] = useState("home")
+
+
+  return (
+    <Menu>
+      <Menu.Item as={Link} to="/" active={active === "home"}
+        content='Home'
+        name='home'>
+      </Menu.Item>
+
+      <Menu.Menu position='right'>
+        <Dropdown item text='Language'>
+          <Dropdown.Menu>
+            <Dropdown.Item>Finnish</Dropdown.Item>
+            <Dropdown.Item>Todo</Dropdown.Item>
+            <Dropdown.Item>Todo</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <Menu.Item>
+          <Button primary>Sign in</Button>
+        </Menu.Item>
+      </Menu.Menu>
+
+
+    </Menu>
+
+  )
+}
