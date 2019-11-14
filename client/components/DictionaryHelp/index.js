dca4e13be13b69a89d602a7bc5e39161c0e9eb72
Koskinen Marko J <markokoskinen2037@gmail.com>
2019-11-14 15:20:53 +0200

    lint

--- client/components/DictionaryHelp/index.js
+++ client/components/DictionaryHelp/index.js
@@ -6,14 +6,14 @@ const DictionaryHelp = () => {
 
   const [showHelp, setShow] = useState(false)
 
-  const wordArray = ['test', 'what', 'is', 'this'].map((word,i) => <List.Item key={i}>{word}</List.Item>) // fix when we get acual data from backend
+  const wordArray = ['test', 'what', 'is', 'this'].map((word, i) => <List.Item key={i}>{word}</List.Item>) // fix when we get acual data from backend
 
   return (
     <Accordion styled fluid>
       <Accordion.Title onClick={() => setShow(!showHelp)} index={0}>
         Dictionary Help
-        </Accordion.Title>
-      <Accordion.Content active={showHelp} index={1} style={{minHeight:'10em'}}>
+      </Accordion.Title>
+      <Accordion.Content active={showHelp} index={1} style={{ minHeight: '10em' }}>
         -- word to translate --
         <List bulleted>
           {wordArray}
