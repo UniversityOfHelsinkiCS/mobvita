6a66f7c806165a1f7879ad19796696f4b14e6cc4
Marko Koskinen <markokoskinen2037@gmail.com>
2019-12-02 14:27:54 +0200

    lint

--- client/components/Bar.js
+++ client/components/Bar.js
@@ -14,103 +14,99 @@ import { logout } from 'Utilities/redux/userReducer'
 
 
 export default function Bar() {
-    const intl = useIntl()
-    const dispatch = useDispatch()
+  const intl = useIntl()
+  const dispatch = useDispatch()
 
-    const [language, setLanguage] = useState('')
+  const [language, setLanguage] = useState('')
 
-    const { user } = useSelector(({ user }) => ({ user: user.data }))
-    const open = useSelector(({ sidebar }) => sidebar.open)
-
-
-    const signOut = () => dispatch(logout())
-    const chooseLanguage = code => () => dispatch(setLocale(code))
+  const { user } = useSelector(({ user }) => ({ user: user.data }))
+  const open = useSelector(({ sidebar }) => sidebar.open)
 
 
-    const menuClickWrapper = (func) => {
-        if (func) func()
+  const signOut = () => dispatch(logout())
+  const chooseLanguage = code => () => dispatch(setLocale(code))
 
-        if (window.innerWidth <= 1024) {
-            dispatch(sidebarSetOpen(false))
-        }
+
+  const menuClickWrapper = (func) => {
+    if (func) func()
+
+    if (window.innerWidth <= 1024) {
+      dispatch(sidebarSetOpen(false))
     }
+  }
 
-    useEffect(() => {
-        if (window.innerWidth >= 1024) {
-            dispatch(sidebarSetOpen(true))
-        } else {
-            dispatch(sidebarSetOpen(false))
-        }
-    }, [])
-
-    useEffect(() => {
-        const currentLanguge = window.location.pathname.split('/')[2]
-        setLanguage(currentLanguge)
-    }, [window.location.pathname])
-
-    if (open) {
-        return (
-            <Swipeable style={{ width: "20em", height: "100vh", position: "absolute" }} onSwipedRight={() => dispatch(sidebarSetOpen(true))} onSwipedLeft={() => dispatch(sidebarSetOpen(false))} trackMouse={true} >
-                <Sidebar
-                    as={Menu}
-                    animation='push'
-                    icon='labeled'
-                    vertical
-                    //onHide={() => dispatch(sidebarSetOpen(false))}
-                    visible={open}
-                    style={{ width: "20em" }}
-                >
-
-                    {user && (
-                        <Menu.Item
-                            onClick={() => menuClickWrapper(signOut)}
-                            icon='log out'
-                            content={user.user.username}
-                        />
-                    )}
-
-                    <Menu.Item
-                        as={Link}
-                        onClick={() => menuClickWrapper()}
-                        to={`/`}
-                        content={intl.formatMessage({ id: 'HOME' })}
-                        icon="home"
-                    />
-
-                    <Menu.Item
-                        as={Link} to={`/stories/${language}#home`}
-                        onClick={() => menuClickWrapper()}
-                        icon="gamepad"
-                        content="Stories"
-                    />
-
-                    <Menu.Item>
-                        <Dropdown text={intl.formatMessage({ id: 'LANGUAGE' })}>
-                            <Dropdown.Menu>
-                                {localeOptions.map(locale => (
-                                    <Dropdown.Item key={locale.code} onClick={chooseLanguage(locale.code)}>
-                                        {locale.name}
-                                    </Dropdown.Item>
-                                ))}
-                            </Dropdown.Menu>
-                        </Dropdown>
-                    </Menu.Item>
-
-                </Sidebar>
-
-
-
-
-            </Swipeable>
-        )
+  useEffect(() => {
+    if (window.innerWidth >= 1024) {
+      dispatch(sidebarSetOpen(true))
     } else {
-        return (
-            <Swipeable onSwipedRight={() => dispatch(sidebarSetOpen(true))}>
-                <Segment onClick={() => dispatch(sidebarSetOpen(true))} style={{ padding: "0.2em", height: "100%", position: "absolute", zIndex: 100, display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
-                    <Icon name="angle right" />
-                </Segment>
-            </Swipeable>
-        )
+      dispatch(sidebarSetOpen(false))
     }
+  }, [])
+
+  useEffect(() => {
+    const currentLanguge = window.location.pathname.split('/')[2]
+    setLanguage(currentLanguge)
+  }, [window.location.pathname])
+
+  if (open) {
+    return (
+      <Swipeable style={{ width: "20em", height: "100vh", position: "absolute" }} onSwipedRight={() => dispatch(sidebarSetOpen(true))} onSwipedLeft={() => dispatch(sidebarSetOpen(false))} trackMouse={true} >
+        <Sidebar
+          as={Menu}
+          animation='push'
+          icon='labeled'
+          vertical
+          //onHide={() => dispatch(sidebarSetOpen(false))}
+          visible={open}
+          style={{ width: "20em" }}
+        >
+
+          {user && (
+            <Menu.Item
+              onClick={() => menuClickWrapper(signOut)}
+              icon='log out'
+              content={user.user.username}
+            />
+          )}
+
+          <Menu.Item
+            as={Link}
+            onClick={() => menuClickWrapper()}
+            to={`/`}
+            content={intl.formatMessage({ id: 'HOME' })}
+            icon="home"
+          />
+
+          <Menu.Item
+            as={Link} to={`/stories/${language}#home`}
+            onClick={() => menuClickWrapper()}
+            icon="gamepad"
+            content="Stories"
+          />
+
+          <Menu.Item>
+            <Dropdown text={intl.formatMessage({ id: 'LANGUAGE' })}>
+              <Dropdown.Menu>
+                {localeOptions.map(locale => (
+                  <Dropdown.Item key={locale.code} onClick={chooseLanguage(locale.code)}>
+                    {locale.name}
+                  </Dropdown.Item>
+                ))}
+              </Dropdown.Menu>
+            </Dropdown>
+          </Menu.Item>
+
+        </Sidebar>
+      </Swipeable>
+    )
+  } else {
+    return (
+      <Swipeable onSwipedRight={() => dispatch(sidebarSetOpen(true))}>
+        <Segment onClick={() => dispatch(sidebarSetOpen(true))} style={{ padding: "0.2em", height: "10%", position: "absolute", zIndex: 100, display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
+          <Icon name="angle right" />
+        </Segment>
+      </Swipeable>
+    )
+  }
 
 }
