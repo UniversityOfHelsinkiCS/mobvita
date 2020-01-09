71ee90bcb359bc9f17b63b787a7cd89bb89b9dd1
Marko Koskinen <markokoskinen2037@gmail.com>
2020-01-09 14:06:52 +0200

    rm swipehandler

--- client/components/App.js
+++ client/components/App.js
@@ -2,15 +2,12 @@ import React from 'react'
 import Router from 'Components/Router'
 
 import Bar from './Bar'
-import SwipeHandler from './SwipeHandler'
 
 /* eslint-disable no-undef */
 // __VERSION__ is defined in webpack
 const App = () => (
   <>
     <Bar />
-
-    <SwipeHandler />
     <div style={{ backgroundColor: '#fafafa' }}>
       <div className="content">
         <Router />
