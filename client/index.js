2da66c3c641a9b07f0a2cd5a18ba0af982564f9b
Atte Haarni <atte.h.1998@gmail.com>
2020-01-14 12:45:18 +0200

    Sentry only in production

--- client/index.js
+++ client/index.js
@@ -8,13 +8,14 @@ import 'react-virtualized/styles.css'
 import 'Assets/custom.scss'
 
 import store from 'Utilities/store'
-import { basePath } from 'Utilities/common'
+import { basePath, inProduction } from 'Utilities/common'
 import App from 'Components/App'
 import ErrorBoundary from 'Components/ErrorBoundary'
 import ConnectedIntlProvider from 'Components/ConnectedIntlProvider'
 
-Sentry.init({ dsn: 'https://509ab4585bb54fda8a94a461c1007146@toska.cs.helsinki.fi/13' })
-
+if (inProduction) {
+  Sentry.init({ dsn: 'https://509ab4585bb54fda8a94a461c1007146@toska.cs.helsinki.fi/13' })
+}
 const refresh = () => render(
   <Provider store={store}>
     <ConnectedIntlProvider>
