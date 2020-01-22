1eb370823ca530e2b191c18ca65c0186a9328d1f
Marko Koskinen <markokoskinen2037@gmail.com>
2020-01-22 12:59:14 +0200

    toaster now checks redux state and shows messages

--- client/components/Toaster.js
+++ client/components/Toaster.js
@@ -2,9 +2,21 @@ import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
 export default function Toaster() {
+  const message = useSelector(({ notification }) => notification.message)
+  const type = useSelector(({ notification }) => notification.type)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (message !== null) {
+      toast(message, { type })
+      dispatch({ type: 'RESET_NOTIFICATION' })
+    }
+  }, [message])
+
   return (
     <ToastContainer position={toast.POSITION.BOTTOM_CENTER} />
   )
