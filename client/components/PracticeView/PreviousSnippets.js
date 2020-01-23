fefa5abe301a8ed44b26bc69ed9a33ac410a7368
Atte Haarni <atte.h.1998@gmail.com>
2020-01-23 16:18:17 +0200

    Previous snippets have line breaks

--- client/components/PracticeView/PreviousSnippets.js
+++ client/components/PracticeView/PreviousSnippets.js
@@ -1,34 +1,41 @@
 import React from 'react'
-import { Segment } from 'semantic-ui-react'
+
+const Word = ({ word, textToSpeech }) => {
+  const { surface, ID, isWrong, tested, lemmas } = word
+  let color = ''
+
+  if (tested) {
+    color = isWrong ? 'firebrick' : 'green'
+  }
+
+  return (
+    <span
+      className="word-interactive "
+      role="button"
+      onClick={() => textToSpeech(surface, lemmas)}
+      key={ID}
+      style={{ color }}
+      onKeyDown={() => textToSpeech(word.surface, word.lemmas)}
+      tabIndex={-1}
+    >
+      {surface}
+    </span>
+  )
+}
 
 const PreviousSnippets = ({ snippets, textToSpeech }) => {
   if (snippets.length === 0) return null
 
   return (
-    <Segment>
-      {snippets.map(snippet => snippet.practice_snippet.map((word) => {
-        const { surface, ID, isWrong, tested, lemmas } = word
-        let color = ''
-
-        if (tested) {
-          color = isWrong ? 'firebrick' : 'green'
-        }
-
-        return (
-          <span
-            className="word-interactive "
-            role="button"
-            onClick={() => textToSpeech(surface, lemmas)}
-            key={ID}
-            style={{ color }}
-            onKeyDown={() => textToSpeech(word.surface, word.lemmas)}
-            tabIndex={-1}
-          >
-            {surface}
-          </span>
-        )
-      }))}
-    </Segment>
+    <div>
+      {snippets.map(snippet => (
+        <p>{
+        snippet.practice_snippet.map(word => (
+          <Word word={word} textToSpeech={textToSpeech} />
+        ))}
+        </p>
+      ))}
+    </div>
   )
 }
 
