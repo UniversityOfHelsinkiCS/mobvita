762ebf3cff7d67eed46a6e5ec007f629eba7edb2
Atte Haarni <atte.h.1998@gmail.com>
2020-01-27 10:54:57 +0200

    Default of english instead of keys, needs a bit more testing

--- client/components/ConnectedIntlProvider.js
+++ client/components/ConnectedIntlProvider.js
@@ -3,7 +3,11 @@ import { IntlProvider } from 'react-intl'
 import translations from 'Utilities/translations'
 
 const mapStateToProps = ({ locale }) => {
-  const messages = translations[locale]
+  // const messages = translations[locale]
+  const messages = locale === 'en' // english is our default, complete locale
+    ? translations.en
+    : { ...translations.en, ...translations[locale] }
+
   return { key: locale, locale, messages }
 }
 
