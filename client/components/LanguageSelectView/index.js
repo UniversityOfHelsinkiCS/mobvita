80a04d20f9999a970e3cb9da98651273ef34479f
Marko Koskinen <markokoskinen2037@gmail.com>
2020-01-29 13:33:06 +0200

    fix bug where new user needs to select language twice

--- client/components/LanguageSelectView/index.js
+++ client/components/LanguageSelectView/index.js
@@ -1,13 +1,44 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Container, Header, Segment } from 'semantic-ui-react'
+import React, { useEffect, useState } from 'react'
+import { Redirect } from 'react-router-dom'
+import { Container, Segment } from 'semantic-ui-react'
 import { FormattedMessage } from 'react-intl'
-import { images, capitalize, supportedLearningLanguages } from 'Utilities/common'
+import { images, capitalize, supportedLearningLanguages, learningLanguageSelector } from 'Utilities/common'
 import { updateLearningLanguage } from 'Utilities/redux/userReducer'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 const LearningLanguageSelectView = () => {
   const dispatch = useDispatch()
+
+  const user = useSelector(({ user }) => user)
+  const learningLanguage = useSelector(learningLanguageSelector)
+  const { pending } = user
+
+  const [learningLanguageChanged, setLearningLanguageChanged] = useState(false)
+  const [waiting, setWaiting] = useState(false)
+
+  useEffect(() => {
+    if (!pending && waiting && learningLanguage) {
+      setLearningLanguageChanged(true)
+      setWaiting(false)
+    }
+  }, [pending])
+
+  const handleLearningLanguageChange = (lang) => {
+    dispatch(updateLearningLanguage(lang))
+    setWaiting(true)
+  }
+
+  if (learningLanguageChanged) {
+    return (
+      <Redirect
+        to={{
+          pathname: '/home',
+          state: { from: '/languageSelectView' },
+        }}
+      />
+    )
+  }
+
 
   return (
     <Container textAlign="center">
@@ -16,10 +47,10 @@ const LearningLanguageSelectView = () => {
       </h2>
       <Segment style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
         {supportedLearningLanguages.map(lang => (
-          <Link key={lang} onClick={() => dispatch(updateLearningLanguage(lang))} to="/home" style={{ width: '7em', display: 'flex', flexDirection: 'column', margin: '1.5em', alignItems: 'center' }}>
+          <div onClick={() => handleLearningLanguageChange(lang)} style={{ width: '7em', display: 'flex', flexDirection: 'column', margin: '1.5em', alignItems: 'center' }}>
             <img src={images[`flag${capitalize(lang.split('-').join(''))}`]} style={{ height: '3em', border: '1px solid whitesmoke' }} alt={lang} />
             <span style={{ color: 'black' }}><FormattedMessage id={lang.split('-').map(l => capitalize(l)).join('-')} /></span>
-          </Link>
+          </div>
         ))}
       </Segment>
     </Container>
