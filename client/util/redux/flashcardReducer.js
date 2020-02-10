59c44a7199b703a7aa41222e38654f3a647dda55
Gert Adamson <adamson.gert@hotmail.com>
2020-02-10 15:24:03 +0200

    reducer for recording flashcard answers

--- client/util/redux/flashcardReducer.js
+++ client/util/redux/flashcardReducer.js
@@ -6,6 +6,13 @@ export const getFlashcards = (inputLanguage, outputLanguage) => {
   return callBuilder(route, prefix, 'get')
 }
 
+export const recordFlashcardAnswer = (inputLanguage, outputLanguage, answerDetails) => {
+  const route = `/flashcards/${inputLanguage}/${outputLanguage}/answer`
+  const prefix = 'RECORD_FLASHCARD_ANSWER'
+  const payload = answerDetails
+  return callBuilder(route, prefix, 'post', payload)
+}
+
 export default (state = {}, action) => {
   switch (action.type) {
     case 'GET_FLASHCARDS_ATTEMPT':
@@ -22,6 +29,24 @@ export default (state = {}, action) => {
         error: false,
       }
     case 'GET_FLASHCARDS_FAILURE':
+      return {
+        ...state,
+        pending: false,
+        error: true,
+      }
+    case 'RECORD_FLASHCARD_ANSWER_ATTEMPT':
+      return {
+        ...state,
+        pending: true,
+        error: false,
+      }
+    case 'RECORD_FLASHCARD_ANSWER_SUCCESS':
+      return {
+        ...state,
+        pending: false,
+        error: false,
+      }
+    case 'RECORD_FLASHCARD_ANSWER_FAILURE':
       return {
         ...state,
         pending: false,
