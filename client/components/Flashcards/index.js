712390b375d583abe57ce26f7c630bbc34f5a721
Gert Adamson <adamson.gert@hotmail.com>
2020-02-12 15:31:46 +0200

    style flashcard next and previous buttons

--- client/components/Flashcards/index.js
+++ client/components/Flashcards/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import SwipeableViews from 'react-swipeable-views'
 import { useDispatch, useSelector } from 'react-redux'
+import { Icon } from 'semantic-ui-react'
 import { getFlashcards } from 'Utilities/redux/flashcardReducer'
 import { learningLanguageSelector, dictionaryLanguageSelector } from 'Utilities/common'
 import Flashcard from './Flashcard'
@@ -25,25 +26,30 @@ const Flashcards = () => {
   }
 
   return (
-    <>
-      <SwipeableViews index={swipeIndex} onChangeIndex={handleIndexChange}>
-        {cards.all.map(card => <Flashcard key={card._id} card={card} />)}
-      </SwipeableViews>
+    <div id="flashcardsContainer">
       <button
         type="button"
         onClick={() => handleIndexChange(swipeIndex - 1)}
         disabled={swipeIndex === 0}
+        className="flashcardNextPreviousButtons"
+        id="flashcardPreviousButton"
+        style={{ marginRight: 0 }}
       >
-        Previous
+        <Icon name="angle double left" size="huge" />
       </button>
+      <SwipeableViews index={swipeIndex} onChangeIndex={handleIndexChange} style={{ width: '30em' }}>
+        {cards.all.map(card => <Flashcard key={card._id} card={card} />)}
+      </SwipeableViews>
       <button
         type="button"
         onClick={() => handleIndexChange(swipeIndex + 1)}
         disabled={swipeIndex === cards.all.length - 1}
+        className="flashcardNextPreviousButtons"
+        id="flashcardNextButton"
       >
-        Next
+        <Icon name="angle double right" size="huge" />
       </button>
-    </>
+    </div>
   )
 }
 
