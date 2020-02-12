4280a6abb8f7fc7ac8fbe2e3cbd5a93341b37087
Marko Koskinen <markokoskinen2037@gmail.com>
2020-02-12 16:09:02 +0200

    show old answer as overlay onclick

--- client/components/PracticeView/PreviousSnippets.js
+++ client/components/PracticeView/PreviousSnippets.js
@@ -1,35 +1,72 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import { Overlay } from 'react-bootstrap'
+import { useIntl } from 'react-intl'
 
-const Word = ({ word, textToSpeech }) => {
+
+const Word = ({ word, textToSpeech, answer }) => {
   const { surface, isWrong, tested, lemmas } = word
-  let color = ''
+  const intl = useIntl()
+  const [show, setShow] = useState(false)
+  const target = useRef(null)
 
+  let color = ''
   if (tested) {
     color = isWrong ? 'firebrick' : 'green'
   }
 
+  const handleClick = () => {
+    setShow(true)
+    textToSpeech(surface, lemmas)
+  }
+
   return (
-    <span
-      className="word-interactive "
-      role="button"
-      onClick={() => textToSpeech(surface, lemmas)}
-      style={{ color }}
-      onKeyDown={() => textToSpeech(word.surface, word.lemmas)}
-      tabIndex={-1}
-    >
-      {surface}
-    </span>
+    <>
+      <span
+        ref={target}
+        className="word-interactive"
+        role="button"
+        onClick={() => handleClick()}
+        style={{ color }}
+        onKeyDown={() => textToSpeech(word.surface, word.lemmas)}
+        tabIndex={-1}
+      >
+        {surface}
+      </span>
+      <Overlay target={target.current} show={show} placement="top" rootClose onHide={() => setShow(false)}>
+        {({
+          placement,
+          scheduleUpdate,
+          arrowProps,
+          outOfBoundaries,
+          show,
+          ...props
+        }) => (
+          <div
+            {...props}
+            style={{
+              backgroundColor: color,
+              padding: '2px 10px',
+              color: 'white',
+              borderRadius: 3,
+              ...props.style,
+            }}
+          >
+            {(answer && answer.users_answer) && tested ? `${intl.formatMessage({ id: 'you-used' })}: ${answer.users_answer}` : 'You did not answer this one'}
+          </div>
+        )}
+      </Overlay>
+    </>
   )
 }
 
-const PreviousSnippets = ({ snippets, textToSpeech }) => {
+const PreviousSnippets = ({ snippets, textToSpeech, answers }) => {
   if (snippets.length === 0) return null
   return (
     <div>
       {snippets.map(snippet => (
         <p key={snippet.snippetid[0]}>{
         snippet.practice_snippet.map(word => (
-          <Word key={word.ID} word={word} textToSpeech={textToSpeech} />
+          <Word answer={answers[word.ID]} key={word.ID} word={word} textToSpeech={textToSpeech} />
         ))}
         </p>
       ))}
