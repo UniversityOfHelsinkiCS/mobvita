33c9e34de9334389cda963de8cbc52ae22e1f865
Gert Adamson <adamson.gert@hotmail.com>
2020-02-18 09:51:30 +0200

    css to buttongroup label

--- client/components/Settings.js
+++ client/components/Settings.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { Modal, Checkbox } from 'semantic-ui-react'
+import { useSelector } from 'react-redux'
+import { Modal } from 'semantic-ui-react'
 import { ButtonGroup, Button } from 'react-bootstrap'
-import { useIntl } from 'react-intl'
 
 const Settings = ({ trigger }) => {
-  const dispatch = useDispatch()
   const { user } = useSelector(({ user }) => ({ user: user.data.user }))
-  const intl = useIntl()
 
 
   return (
@@ -16,7 +13,7 @@ const Settings = ({ trigger }) => {
         Learning settings
       </Modal.Header>
       <Modal.Content className="flex" style={{ display: 'flex', flexDirection: 'column' }}>
-        <label htmlFor="difficultyButtons">Exercise difficulty level</label>
+        <span className="label">Exercise difficulty level</span>
         <ButtonGroup name="difficultyButtons" size="lg">
           <Button>A1</Button>
           <Button>A2</Button>
