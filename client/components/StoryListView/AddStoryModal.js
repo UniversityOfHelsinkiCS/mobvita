cb588d738ee2e744c0c87140e9d7173a342bcd66
Atte Haarni <atte.h.1998@gmail.com>
2020-02-19 16:47:12 +0200

    Added placeholder to story text field

--- client/components/StoryListView/AddStoryModal.js
+++ client/components/StoryListView/AddStoryModal.js
@@ -5,9 +5,11 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Button } from 'react-bootstrap'
 import { learningLanguageSelector, capitalize } from 'Utilities/common'
 import { postStory } from 'Utilities/redux/uploadProgressReducer'
+import { useIntl } from 'react-intl'
 
 
 const AddStoryModal = ({ trigger }) => {
+  const intl = useIntl()
   const [text, setText] = useState('')
 
   const learningLanguage = useSelector(learningLanguageSelector)
@@ -20,6 +22,7 @@ const AddStoryModal = ({ trigger }) => {
     }
     dispatch(postStory(newStory))
   }
+
   return (
     <Modal
       dimmer="inverted"
@@ -29,7 +32,12 @@ const AddStoryModal = ({ trigger }) => {
       <Modal.Header><FormattedMessage id="add-story" /></Modal.Header>
       <Modal.Content>
         <span>Add a text</span>
-        <textarea className="story-text-input" value={text} onChange={e => setText(e.target.value)} />
+        <textarea
+          placeholder={intl.formatMessage({ id: 'title-on-first-line-text-after-an-empty-line-f2c9245edf5d0379b77008c6db1fd5a9' })}
+          className="story-text-input"
+          value={text}
+          onChange={e => setText(e.target.value)}
+        />
         <Button variant="primary" onClick={addStory}><FormattedMessage id="Confirm" /></Button>
       </Modal.Content>
 
