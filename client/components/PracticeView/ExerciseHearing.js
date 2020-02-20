7918f584883a4c094c2d0c8da10249b8da06e3da
Marko Koskinen <markokoskinen2037@gmail.com>
2020-02-20 14:02:22 +0200

    make it look the same as exercicecloze

--- client/components/PracticeView/ExerciseHearing.js
+++ client/components/PracticeView/ExerciseHearing.js
@@ -1,12 +1,12 @@
 import React, { createRef, useState, useEffect } from 'react'
-import { Input, Icon } from 'semantic-ui-react'
+import { Icon } from 'semantic-ui-react'
 import { getTextWidth } from 'Utilities/common'
 
 
 const ExerciseHearing = ({ word, handleClick, handleChange, value }) => {
   const [className, setClassname] = useState('hearing untouched')
   const [touched, setTouched] = useState(false)
-  const inputRef = createRef()
+  const inputRef = createRef(null)
 
   const { isWrong, tested } = word
 
@@ -34,23 +34,27 @@ const ExerciseHearing = ({ word, handleClick, handleChange, value }) => {
   }
 
   return (
-    <Input
-      data-cy="exercise-hearing"
-      ref={inputRef}
-      key={word.ID}
-      onChange={e => handle(e, word)}
-      value={value}
-      icon={<Icon name="volume up" link onClick={() => clickHandler(word.surface)} style={{ marginRight: '4px' }} />}
-      transparent
-      className={className}
-      style={{
-        width: getTextWidth(word.surface),
-        minWidth: getTextWidth(word.surface),
-        marginRight: '2px',
-        height: '1.5em',
-        borderRadius: '6px',
-      }}
-    />
+    <span>
+      <input
+        data-cy="exercise-hearing"
+        ref={inputRef}
+        key={word.ID}
+        onChange={e => handle(e, word)}
+        value={value}
+        onFocus={() => clickHandler(word.surface)}
+        transparent
+        className={className}
+        style={{
+          width: getTextWidth(word.surface),
+          minWidth: getTextWidth(word.surface),
+          marginRight: '2px',
+          height: '1.5em',
+          borderRadius: '6px',
+          lineHeight: 'normal',
+        }}
+      />
+      <Icon name="volume up" link onClick={() => clickHandler(word.surface)} style={{ marginLeft: '-25px' }} />
+    </span>
   )
 }
 
