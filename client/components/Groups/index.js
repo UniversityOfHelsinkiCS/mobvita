04e030788223d27d716558cc72f1c3b4a3e30681
Gert Adamson <adamson.gert@hotmail.com>
2020-02-21 13:41:38 +0200

    style group view

--- client/components/Groups/index.js
+++ client/components/Groups/index.js
@@ -1,7 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getGroups, addStudentsToGroup } from 'Utilities/redux/groupsReducer'
-import { Dropdown, Form } from 'react-bootstrap'
+import {
+  Dropdown,
+  Accordion,
+  Card,
+  ListGroup,
+  InputGroup,
+  Button,
+  FormControl,
+} from 'react-bootstrap'
 
 const GroupView = () => {
   const [currentGroupId, setCurrentGroupId] = useState(null)
@@ -31,28 +39,56 @@ const GroupView = () => {
   const currentGroup = groups.find(group => group.group_id === currentGroupId)
 
   return (
-    <div className="" style={{ }}>
-      <Dropdown onSelect={key => setCurrentGroupId(key)}>
+    <div className="maxContentSize autoMargin">
+      <Dropdown onSelect={key => setCurrentGroupId(key)} style={{ marginBottom: '1em' }}>
         <Dropdown.Toggle variant="info" id="dropdown-basic">
           {currentGroup.groupName}
         </Dropdown.Toggle>
-
         <Dropdown.Menu>
-          {groups.map(group => <Dropdown.Item eventKey={group.group_id}>{group.groupName}</Dropdown.Item>)}
+          {groups.map(group => (
+            <Dropdown.Item eventKey={group.group_id}>{group.groupName}</Dropdown.Item>
+          ))}
         </Dropdown.Menu>
       </Dropdown>
 
-      <div>{currentGroup.groupName}</div>
-      <div>Teachers:</div>
-      <ul>
-        {currentGroup.teachers.map(teacher => <li key={teacher.userName}>{teacher.userName}</li>)}
-      </ul>
-      <div>Students:</div>
-      <ul>
-        {currentGroup.students.map(student => <li key={student.userName}>{student.userName}</li>)}
-      </ul>
-      <input type="text" value={studentsToAdd} onChange={() => setStudentsToAdd(event.target.value)} />
-      <input type="button" value="add students" onClick={addStudents} />
+      <Accordion style={{ marginBottom: '1em' }}>
+        <Card>
+          <Accordion.Toggle as={Card.Header} eventKey="0">
+            Teachers
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey="0">
+            <ListGroup>
+              {currentGroup.teachers.map(teacher => (
+                <ListGroup.Item key={teacher.userName}>{teacher.userName}</ListGroup.Item>
+              ))}
+            </ListGroup>
+          </Accordion.Collapse>
+        </Card>
+        <Card>
+          <Accordion.Toggle as={Card.Header} eventKey="1">
+            Students
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey="1">
+            <ListGroup>
+              {currentGroup.students.map(student => (
+                <ListGroup.Item key={student.userName}>{student.userName}</ListGroup.Item>
+              ))}
+            </ListGroup>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
+
+      <InputGroup className="mb-3">
+        <FormControl
+          as="textarea"
+          placeholder="Student emails"
+          value={studentsToAdd}
+          onChange={event => setStudentsToAdd(event.target.value)}
+        />
+        <InputGroup.Append>
+          <Button variant="primary" onClick={addStudents}>Add students</Button>
+        </InputGroup.Append>
+      </InputGroup>
     </div>
   )
 }
