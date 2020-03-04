18a77ef9d742340084ecbeffefc91d0b2a0eebad
Marko Koskinen <markokoskinen2037@gmail.com>
2020-03-04 16:43:09 +0200

    remove shake.

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Navbar } from 'react-bootstrap'
 import Headroom from 'react-headroom'
 import { Icon } from 'semantic-ui-react'
-import { Shake } from 'reshake'
 import { sidebarSetOpen } from 'Utilities/redux/sidebarReducer'
 import useWindowDimensions from 'Utilities/windowDimensions'
 
@@ -12,17 +11,6 @@ export default function NavBar({ history }) {
   const { user } = useSelector(({ user }) => ({ user: user.data }))
   const open = useSelector(({ sidebar }) => sidebar.open)
   const dispatch = useDispatch()
-
-  const [eloChanged, setEloChanged] = useState(false)
-
-  useEffect(() => {
-    if (user && user.user.exercise_history) {
-      setEloChanged(true)
-      setTimeout(() => {
-        setEloChanged(false)
-      }, 1000)
-    }
-  }, [user])
 
   const elo = (user && user.user.exercise_history && user.user.exercise_history[user.user.exercise_history.length - 1] && user.user.exercise_history[user.user.exercise_history.length - 1].score)
     ? user.user.exercise_history[user.user.exercise_history.length - 1].score
@@ -58,22 +46,10 @@ export default function NavBar({ history }) {
         )
         }
         {user && (
-        <Shake
-          h={5}
-          v={5}
-          r={3}
-          dur={500}
-          int={10}
-          max={100}
-          fixed
-          fixedStop={false}
-          freez={false}
-          active={eloChanged}
-        >
+
           <Navbar.Text style={{ color: 'white', marginRight: '1em' }}>
             <div>{`Points: ${elo}`}</div>
           </Navbar.Text>
-        </Shake>
         )}
       </Navbar>
     </Headroom>
