9f288df6d7c2300a4057d9b4f72dd12dfa6f4d87
Marko Koskinen <markokoskinen2037@gmail.com>
2020-03-09 11:42:20 +0200

    fix sentry issue #1902

--- client/components/PracticeView/CurrentPractice.js
+++ client/components/PracticeView/CurrentPractice.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getCurrentSnippet, getNextSnippet, postAnswers, setTotalNumberAction } from 'Utilities/redux/snippetsReducer'
 import { getTranslationAction, clearTranslationAction } from 'Utilities/redux/translationReducer'
-import { capitalize, learningLanguageSelector } from 'Utilities/common'
+import { capitalize, learningLanguageSelector, translatableLanguages } from 'Utilities/common'
 
 import PreviousSnippets from 'Components/PracticeView/PreviousSnippets'
 import ExerciseCloze from 'Components/PracticeView/ExerciseCloze'
@@ -155,7 +155,7 @@ const CurrentPractice = ({ storyId }) => {
         getTranslationAction(
           capitalize(learningLanguage),
           wordLemmas,
-          capitalize(dictionaryLanguage),
+          capitalize(dictionaryLanguage || translatableLanguages[learningLanguage][0]),
           storyId,
           wordId,
         ),
