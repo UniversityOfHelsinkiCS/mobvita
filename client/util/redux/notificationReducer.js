d0cc16954dc210584800e296cb65f507ec8254f5
atte <atte.h.1998@gmail.com>
2020-03-31 10:50:46 +0300

    Actually commited merge

--- client/util/redux/notificationReducer.js
+++ client/util/redux/notificationReducer.js
@@ -104,7 +104,6 @@ export default (state = initialState, action) => {
         message: action.response.response.data,
         type: type.error,
       }
-<<<<<<< HEAD
     case 'FORGOT_PASSWORD_SUCCESS':
       return {
         message: action.response.response.data,
@@ -116,14 +115,12 @@ export default (state = initialState, action) => {
         type: type.error,
       }
 
-=======
     case 'GET_SNIPPET_ANSWERS_FAILURE':
       return {
         translationId: 'post-answers-error',
         type: type.error,
         options: { autoClose: 10000 },
       }
->>>>>>> 9ac3d3655a332a95646cff73005877e4f64ab6f5
     default:
       return state
   }
