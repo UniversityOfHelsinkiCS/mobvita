c48eb5eab9cee046bf8c8485044c394db5c42355
Jami Kousa <jami.kousa@helsinki.fi>
2020-04-15 15:28:03 +0300

    rename isDeployed to inProduction enabling sentry

--- config/common.js
+++ config/common.js
@@ -2,14 +2,14 @@
  * Insert application wide common items here
  */
 
-const isDeployed = process.env.NODE_ENV === 'production'
+const inProduction = process.env.NODE_ENV === 'production'
 
 const basePath = process.env.BASE_PATH || '/'
 
-const hiddenFeatures = !isDeployed || basePath === '/staging'
+const hiddenFeatures = !inProduction || basePath === '/staging'
 
 module.exports = {
-  isDeployed,
+  inProduction,
   basePath,
   hiddenFeatures,
 }
