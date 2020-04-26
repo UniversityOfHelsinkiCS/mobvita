747a0e6c090457ffb176d24ce10059bce3d2e9b4
Roman Yangarber <Roman.Yangarber@cs.helsinki.fi>
2020-04-26 13:02:23 +0300

    fix .JS code for re-enabling listening exercises

--- client/components/Profile/Settings.js
+++ client/components/Profile/Settings.js
@@ -47,7 +47,7 @@ const Settings = () => {
           onChange={() => dispatch(updateAudioTask(!user.task_audio))}
           disabled={pending}
         />
-        <!-- <span style={{ color: 'gray' }}><i>Temporarily unavailable due to technical problem</i></span> -->
+        <span style={{ display: 'none', color: 'gray' }}><i>Temporarily unavailable due to technical problem</i></span>
         <SettingToggle
           translationId="second-chance-when-practicing-stories"
           checked={user.second_try}
