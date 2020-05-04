46369ce6f8274f2fe6d36de717ca25d3d1e2215f
Gert Adamson <adamson.gert@hotmail.com>
2020-05-04 08:01:42 +0300

    remove unused proxy controllers

--- server/controllers/emailController.js
+++
@@ -1,9 +0,0 @@
-const { axios } = require('@util/common')
-
-const sendEmail = async (req, res) => {
-  const url = '/contact'
-  const response = await axios.post(url, req.body, { headers: req.headers })
-  res.send(response.data)
-}
-
-module.exports = { sendEmail }
--- server/controllers/fallbackController.js
+++
@@ -1,5 +0,0 @@
-const unknown = (req, res) => {
-  res.status(404).send({ error: 'Revita: no such endpoint ' })
-}
-
-module.exports = { unknown }
