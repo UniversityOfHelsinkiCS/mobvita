b4f80585e6e074870ef5e3d4bd79c15edaf76654
Gert Adamson <adamson.gert@hotmail.com>
2020-05-06 12:22:09 +0300

    create syriac keyboard layout

--- client/components/PracticeView/KeyboardLayouts.js
+++ client/components/PracticeView/KeyboardLayouts.js
@@ -39,6 +39,27 @@ export const keyboardLayouts = {
       },
     },
   ],
+  Syriac: [
+    {
+      name: 'Syriac',
+      layout: {
+        default: [
+          '\u070f 1 2 3 4 5 6 7 8 9 0 - = {backspace}',
+          '{tab} \u0714 \u0728 \u0716 \u0729 \u0726 \u071c \u0725 \u0717 \u071e \u071a \u0713 \u0715 \u0706',
+          '{capslock} \u072b \u0723 \u071d \u0712 \u0720 \u0710 \u072c \u0722 \u0721 \u071f \u071b {enter}',
+          '{shift} \u0706 ] [ \u0724 \u072a \u0727 \u0700 \u002e \u0718 \u0719 \u0707 {shift}',
+          '{space}',
+        ],
+        shift: [
+          '\u032e \u0021 \u030a \u0325 \u0749 \u2670 \u2671 \u070a \u00bb \u0029 \u0028 \u00ab \u002b {backspace}',
+          '{tab} \u0730 \u0733 \u0736 \u073a \u073d \u0740 \u0741 \u0308 \u0304 \u0307 \u0303 \u074a \u003a',
+          '{capslock} \u0731 \u0734 \u0737 \u073b \u073e \u0711 \u0640 \u0324 \u0331 \u0323 \u0330 {enter}',
+          '{shift} \u003a \u0732 \u0735 \u0738 \u073c \u073f \u0739 \u0742 \u060c \u061b \u061f {shift}',
+          '{space}',
+        ],
+      },
+    },
+  ],
 }
 
 export default keyboardLayouts
