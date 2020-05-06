cf201fa068be2926f717795f541f1356f9a610bf
Gert Adamson <adamson.gert@hotmail.com>
2020-05-06 09:01:16 +0300

    possible TypeError fix

--- client/components/ConnectedIntlProvider.js
+++ client/components/ConnectedIntlProvider.js
@@ -6,7 +6,7 @@ import { localeNameToCode } from '../util/common'
 const mapStateToProps = ({ locale, user }) => {
   let actualLocale = locale
 
-  if (user.data && user.data.user.interfaceLanguage) { // If user has logged in, use locale from user object, else use value from localeReducer
+  if (user.data && user.data.user && user.data.user.interfaceLanguage) { // If user has logged in, use locale from user object, else use value from localeReducer
     actualLocale = localeNameToCode(user.data.user.interfaceLanguage)
   }
 
