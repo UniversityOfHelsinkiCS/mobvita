8c4260bc28633b80ed5dafe3e5f1715269ca4439
Darake <adamson.gert@hotmail.com>
2020-05-20 14:11:28 +0300

    refetch user when entering progress page

--- client/components/Profile/Progress/index.js
+++ client/components/Profile/Progress/index.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState, useEffect, shallowEqual } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import DatePicker from 'react-datepicker'
 import moment from 'moment'
 import { FormattedMessage } from 'react-intl'
+import { getSelf } from 'Utilities/redux/userReducer'
 import ProgressGraph from 'Components/ProgressGraph'
+import Spinner from 'Components/Spinner'
 import 'react-datepicker/dist/react-datepicker.css'
 import ProgressStats from './ProgressStats'
 
@@ -19,10 +21,20 @@ const Progress = () => {
   const [startDate, setStartDate] = useState(moment().subtract(2, 'months').toDate())
   const [endDate, setEndDate] = useState(moment().toDate())
 
-  const {
-    exercise_history: exerciseHistory,
-    flashcard_history: flashcardHistory,
-  } = useSelector(({ user }) => user.data.user)
+  const { exerciseHistory, flashcardHistory, pending } = useSelector(({ user }) => {
+    const exerciseHistory = user.data.user.exercise_history
+    const flashcardHistory = user.data.user.flashcard_history
+    const { pending } = user
+    return { exerciseHistory, flashcardHistory, pending }
+  }, shallowEqual)
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getSelf())
+  }, [])
+
+  if (pending || pending === undefined) return <Spinner />
 
   return (
     <div className="component-container">
