ff03b7e0adcafdf68f38dabe33a3adc8c5b9e193
atte <atte.h.1998@gmail.com>
2020-06-11 17:17:58 +0300

    Small cypress tweaks

--- cypress/integration/tests_spec.js
+++ cypress/integration/tests_spec.js
@@ -6,10 +6,9 @@ describe('test view', function() {
   this.beforeEach(function () {
     cy.loginExisting().as('user')
     cy.visit('http://localhost:8000')
-    cy.get('[data-cy=hamburger]').click()
   })
 
-  it('can start a new test and answer a question', function() {
+  it('can start a new test', function() {
     cy.get('[data-cy=test-tab]').click()
     cy.url().should('include', '/tests')
     cy.get('[data-cy=start-test]').click()
