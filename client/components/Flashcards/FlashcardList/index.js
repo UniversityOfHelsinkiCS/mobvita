2824b8b2dfa4906cd7ebc919524a4629a5463198
Darake <adamson.gert@hotmail.com>
2020-06-16 12:20:08 +0300

    flashcard deletion on list view

--- client/components/Flashcards/FlashcardList/index.js
+++ client/components/Flashcards/FlashcardList/index.js
@@ -1,10 +1,23 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { ListGroup } from 'react-bootstrap'
+import { Icon } from 'semantic-ui-react'
+import { deleteFlashcard } from 'Utilities/redux/flashcardReducer'
 
-const ListItem = ({ lemma }) => (
-  <ListGroup.Item>{lemma}</ListGroup.Item>
-)
+const ListItem = ({ lemma, id }) => {
+  const dispatch = useDispatch()
+
+  const handleDelete = () => {
+    dispatch(deleteFlashcard(id))
+  }
+
+  return (
+    <ListGroup.Item>
+      {lemma}
+      <Icon name="close" onClick={handleDelete} />
+    </ListGroup.Item>
+  )
+}
 
 const FlashcardList = () => {
   const cards = useSelector(state => state.flashcards.cards)
@@ -12,7 +25,7 @@ const FlashcardList = () => {
 
   return (
     <ListGroup>
-      {cards.map(card => <ListItem key={card._id} lemma={card.lemma} />)}
+      {cards.map(card => <ListItem key={card._id} id={card._id} lemma={card.lemma} />)}
     </ListGroup>
   )
 }
