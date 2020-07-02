2133c876fbb51f9521fab3971a24e31f01a977fc
atte <atte.h.1998@gmail.com>
2020-07-02 19:15:57 +0300

    Focus to next in text

--- client/components/CrosswordView/index.js
+++ client/components/CrosswordView/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { hiddenFeatures } from 'Utilities/common'
@@ -10,6 +10,7 @@ import PlainWord from 'Components/PracticeView/PlainWord'
 const CrosswordView = () => {
   const { storyId } = useParams()
   const crosswordRef = useRef()
+  const [currentClue, setCurrentClue] = useState(null)
   const dispatch = useDispatch()
 
   const { pending, data } = useSelector(({ crossword }) => crossword)
@@ -32,13 +33,33 @@ const CrosswordView = () => {
   ), {})
 
   const moveTo = (clue) => {
-    crosswordRef.current.moveTo(clue.clue_direction, clue.clue_number)
+    setCurrentClue(clue)
   }
+
+  useEffect(() => {
+    if (!currentClue) return
+    crosswordRef.current.moveTo(currentClue.clue_direction, currentClue.clue_number)
+  }, [currentClue])
+
 
   const clues = data.clue && data.clue.map((clue) => {
     if (clue.clue_number) return <span onClick={() => moveTo(clue)} key={clue.ID}><b>{clue.clue_number}. {clue.clue_direction}</b></span>
     return <PlainWord key={clue.ID} surface={clue.surface} lemmas={clue.lemmas} wordId={clue.ID}>{clue.surface}</PlainWord>
   })
+
+  const handleTabPress = ({ key }) => {
+    if (key !== 'Tab') return
+    const index = data.clue.findIndex(clue => clue.ID === currentClue.ID)
+    const nextClue = data.clue.slice(index + 1).find(clue => clue.clue_number)
+
+    moveTo(nextClue)
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleTabPress)
+    return () => document.removeEventListener('keydown', handleTabPress)
+  }, [currentClue, data])
+
 
   if (!hiddenFeatures) return null
 
@@ -47,7 +68,7 @@ const CrosswordView = () => {
   return (
     <div style={{ display: 'flex' }}>
       <Crossword data={formattedData} useStorage={false} ref={crosswordRef} />
-      <div style={{ display: 'inline-block', width: '600px', maxHeight: '100%', overflow: 'scroll' }}>{clues}</div>
+      <div style={{ width: '600px', height: '600px', overflow: 'scroll' }}>{clues}</div>
     </div>
   )
 }
