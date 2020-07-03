e77f608aded542e1a384298f0965c591a0107627
atte <atte.h.1998@gmail.com>
2020-07-03 10:33:49 +0300

    cache crossword to localstorage for easier development

--- client/components/CrosswordView/index.js
+++ client/components/CrosswordView/index.js
@@ -6,28 +6,41 @@ import { getCrossword } from 'Utilities/redux/crosswordReducer'
 import Crossword from '@ajhaa/react-crossword'
 import Spinner from 'Components/Spinner'
 import PlainWord from 'Components/PracticeView/PlainWord'
+import { isEmpty } from 'lodash'
 
 const CrosswordView = () => {
   const { storyId } = useParams()
   const crosswordRef = useRef()
   const [currentClue, setCurrentClue] = useState(null)
+  const [data, setData] = useState()
   const dispatch = useDispatch()
 
-  const { pending, data } = useSelector(({ crossword }) => crossword)
+  const { data: crosswordData } = useSelector(({ crossword }) => crossword)
 
   useEffect(() => {
-    dispatch(getCrossword(storyId))
+    localStorage.removeItem('guesses')
+    const fromStorage = localStorage.getItem('crossword')
+    if (fromStorage) {
+      console.log(fromStorage)
+      setData(JSON.parse(fromStorage))
+    } else {
+      dispatch(getCrossword(storyId))
+    }
   }, [])
 
   useEffect(() => {
-  }, [crosswordRef.current])
+    if (crosswordData && !isEmpty(crosswordData)) {
+      localStorage.setItem('crossword', JSON.stringify(crosswordData))
+      setData(crosswordData)
+    }
+  }, [crosswordData])
 
-  const formattedData = data.entries && data.entries.reduce((newData, entry) => (
+  const formattedData = data?.entries?.reduce((newData, entry) => (
     {
       ...newData,
       [entry.direction]: {
         ...newData[entry.direction],
-        [entry.number]: { answer: entry.answer, row: entry.position.x, col: entry.position.y, clue: 'THIS IS A CLUE' },
+        [entry.number]: { answer: entry.answer.toUpperCase(), row: entry.position.x, col: entry.position.y, clue: 'none' },
       },
     }
   ), {})
@@ -37,7 +50,7 @@ const CrosswordView = () => {
     setCurrentClue(data.clue.find(clue => clue.clue_number === Number(number)))
   }
 
-  const clues = data.clue && data.clue.map((clue) => {
+  const clues = data?.clue?.map((clue) => {
     if (clue.clue_number) {
       return (
         <span
@@ -72,12 +85,17 @@ const CrosswordView = () => {
 
   if (!hiddenFeatures) return null
 
-  if (pending || !formattedData) return <Spinner />
+  if (!formattedData) return <Spinner />
 
   return (
-    <div style={{ display: 'flex' }}>
-      <Crossword onWordChange={handleWordChange} data={formattedData} useStorage={false} ref={crosswordRef} />
-      <div style={{ width: '600px', height: '600px', overflow: 'scroll' }}>{clues}</div>
+    <div style={{ display: 'flex', height: '70%' }}>
+      <Crossword
+        onWordChange={handleWordChange}
+        onCorrect={(...e) => console.log(e)}
+        data={formattedData}
+        ref={crosswordRef}
+      />
+      <div style={{ width: '600px', overflow: 'scroll' }}>{clues}</div>
     </div>
   )
 }
