b7adc361a03febf9507ca6151db6d278c8a4bf03
Darake <adamson.gert@hotmail.com>
2020-07-10 17:08:47 +0300

    clear practice modal timeout on unmount

--- client/components/HomeView/PracticeModal.js
+++ client/components/HomeView/PracticeModal.js
@@ -7,46 +7,37 @@ import CheckboxGroup from 'Components/CheckboxGroup'
 import { FormattedMessage } from 'react-intl'
 import { Button, Spinner } from 'react-bootstrap'
 
-
-const extractFilters = object => Object
-  .entries(object)
-  .filter(entry => entry[1])
-  .map(([key]) => capitalize(key))
+const extractFilters = object =>
+  Object.entries(object)
+    .filter(entry => entry[1])
+    .map(([key]) => capitalize(key))
 
 const PracticeModal = ({ trigger }) => {
-  const [libraries, setLibraries] = useState(
-    {
-      public: true,
-      private: true,
-      group: true,
-    },
-  )
-  const [categories, setCategories] = useState(
-    {
-      culture: true,
-      politics: true,
-      science: true,
-      sport: true,
-      uncategorized: true,
-    },
-  )
+  const [libraries, setLibraries] = useState({
+    public: true,
+    private: true,
+    group: true,
+  })
+  const [categories, setCategories] = useState({
+    culture: true,
+    politics: true,
+    science: true,
+    sport: true,
+    uncategorized: true,
+  })
   const [filteredStories, setFilteredStories] = useState([])
 
   const [randomStoryIndex, setRandom] = useState(0)
 
-  const { stories, pending } = useSelector(({ stories }) => (
-    {
-      stories: stories.data,
-      pending: stories.pending,
-    }
-  ))
+  const { stories, pending } = useSelector(({ stories }) => ({
+    stories: stories.data,
+    pending: stories.pending,
+  }))
 
-  const { user, refreshed } = useSelector(({ user }) => (
-    {
-      user: user.data,
-      refreshed: user.refreshed,
-    }
-  ))
+  const { user, refreshed } = useSelector(({ user }) => ({
+    user: user.data,
+    refreshed: user.refreshed,
+  }))
 
   const [waiting, setWaiting] = useState(true)
   const [temp, setTemp] = useState(null)
@@ -60,10 +51,14 @@ const PracticeModal = ({ trigger }) => {
       if (temp) {
         clearTimeout(temp)
       }
-      setTemp(setTimeout(() => {
-        setWaiting(false)
-      }, 500))
+      setTemp(
+        setTimeout(() => {
+          setWaiting(false)
+        }, 500)
+      )
     }
+
+    return clearTimeout(temp)
   }, [pending, refreshed])
 
   // preload practice modal images
@@ -74,32 +69,33 @@ const PracticeModal = ({ trigger }) => {
     new Image().src = images.science1
   }, [])
 
-
   useEffect(() => {
     const librariesToShow = extractFilters(libraries)
     const categoriesToShow = extractFilters(categories)
 
-    const filtered = stories.filter((story) => {
-      if (story.public) {
-        return librariesToShow.includes('Public')
-      }
+    const filtered = stories
+      .filter(story => {
+        if (story.public) {
+          return librariesToShow.includes('Public')
+        }
 
-      if (story.sharedwith && story.sharedwith.includes(user.user.oid)) {
+        if (story.sharedwith && story.sharedwith.includes(user.user.oid)) {
+          return librariesToShow.includes('Private')
+        }
+
+        if (story.user !== user.user.oid) {
+          return librariesToShow.includes('Group')
+        }
+
         return librariesToShow.includes('Private')
-      }
+      })
+      .filter(story => {
+        if (categoriesToShow.includes('Uncategorized') && !story.category) {
+          return true
+        }
 
-      if (story.user !== user.user.oid) {
-        return librariesToShow.includes('Group')
-      }
-
-      return librariesToShow.includes('Private')
-    }).filter((story) => {
-      if (categoriesToShow.includes('Uncategorized') && !story.category) {
-        return true
-      }
-
-      return categoriesToShow.includes(story.category)
-    })
+        return categoriesToShow.includes(story.category)
+      })
 
     if (filtered.length > 0) {
       const random = Math.ceil(Math.random() * filtered.length) - 1
@@ -109,14 +105,13 @@ const PracticeModal = ({ trigger }) => {
     setFilteredStories(filtered)
   }, [stories, libraries, categories])
 
-
   let filteredLink = ''
 
   if (filteredStories.length > 0) {
     filteredLink = `/stories/${filteredStories[randomStoryIndex]._id}/practice`
   }
 
-  const handleLibraryChange = (library) => {
+  const handleLibraryChange = library => {
     setLibraries({ ...libraries, [library]: !libraries[library] })
   }
 
@@ -135,64 +130,72 @@ const PracticeModal = ({ trigger }) => {
     })
   }
 
-  const toggleCategory = (category) => {
+  const toggleCategory = category => {
     setCategories({ ...categories, [category]: !categories[category] })
   }
 
   return (
-    <Modal
-      dimmer="inverted"
-      closeIcon
-      trigger={trigger}
-      onClose={handleClose}
-    >
-      <Modal.Header><FormattedMessage id="practice" /></Modal.Header>
+    <Modal dimmer="inverted" closeIcon trigger={trigger} onClose={handleClose}>
+      <Modal.Header>
+        <FormattedMessage id="practice" />
+      </Modal.Header>
       <Modal.Content className="practiceModal">
-
         <div>
           <Link to={filteredLink}>
             {waiting ? (
               <Spinner animation="border" variant="primary" />
-            )
-              : (
-                <Button variant="primary" data-cy="start-random" disabled={!filteredLink}>
-                  <FormattedMessage id="practice-random-story" />{` (${filteredStories.length} `}<FormattedMessage id="stories-selected" />)
-                </Button>
-              )
-            }
+            ) : (
+              <Button variant="primary" data-cy="start-random" disabled={!filteredLink}>
+                <FormattedMessage id="practice-random-story" />
+                {` (${filteredStories.length} `}
+                <FormattedMessage id="stories-selected" />)
+              </Button>
+            )}
           </Link>
         </div>
 
         <div>
-          <div><FormattedMessage id="Library" /></div>
+          <div>
+            <FormattedMessage id="Library" />
+          </div>
           <CheckboxGroup
             values={libraries}
             onClick={handleLibraryChange}
             additionalClass="wrap-and-grow"
             reverse
           />
-
         </div>
         <div>
-          <div><FormattedMessage id="Category" /></div>
-          <div className="checkbox-group" style={{ flexWrap: 'wrap' }} data-cy="practice-categories">
-            {Object.entries(categories).sort().slice(0, 4).map(([name, enabled]) => (
-              <Button
-                style={{
-                  backgroundImage: `url(${images[name + 1]})`,
-                  height: '13em',
-                  backgroundSize: 'cover',
-                  backgroundPosition: 'center',
-                  flexBasis: '50%',
-                  border: '1px solid white',
-                }}
-                onClick={() => toggleCategory(name)}
-                className={!enabled && 'disabled'}
-                key={name}
-              >
-                <span style={{ fontWeight: '1000' }}><FormattedMessage id={capitalize(name)} /></span>
-              </Button>
-            ))}
+          <div>
+            <FormattedMessage id="Category" />
+          </div>
+          <div
+            className="checkbox-group"
+            style={{ flexWrap: 'wrap' }}
+            data-cy="practice-categories"
+          >
+            {Object.entries(categories)
+              .sort()
+              .slice(0, 4)
+              .map(([name, enabled]) => (
+                <Button
+                  style={{
+                    backgroundImage: `url(${images[name + 1]})`,
+                    height: '13em',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    flexBasis: '50%',
+                    border: '1px solid white',
+                  }}
+                  onClick={() => toggleCategory(name)}
+                  className={!enabled && 'disabled'}
+                  key={name}
+                >
+                  <span style={{ fontWeight: '1000' }}>
+                    <FormattedMessage id={capitalize(name)} />
+                  </span>
+                </Button>
+              ))}
           </div>
           <Button
             block
