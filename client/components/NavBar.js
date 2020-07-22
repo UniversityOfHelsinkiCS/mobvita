dbbea1a84081c84a970b0125d1f8ea9274f7a171
atte <atte.h.1998@gmail.com>
2020-07-22 12:18:14 +0300

    Make navbar brand a link, format Navbar file

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { Navbar } from 'react-bootstrap'
 import Headroom from 'react-headroom'
 import { Icon } from 'semantic-ui-react'
-import { useHistory } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { sidebarSetOpen } from 'Utilities/redux/sidebarReducer'
 import { useIntl } from 'react-intl'
 import useWindowDimensions from 'Utilities/windowDimensions'
@@ -28,28 +28,25 @@ export default function NavBar() {
   const showStoryElo = history.location.pathname.includes('practice')
   const showFlashcardElo = history.location.pathname.includes('flashcards')
 
-  const storyElo = (user && user.user.exercise_history && user.user.exercise_history.length > 0)
-    ? user.user.exercise_history[user.user.exercise_history.length - 1].score
-    : 0
+  const storyElo =
+    user && user.user.exercise_history && user.user.exercise_history.length > 0
+      ? user.user.exercise_history[user.user.exercise_history.length - 1].score
+      : 0
 
-  const flashcardElo = (user && user.user.flashcard_history && user.user.flashcard_history.length > 0)
-    ? user.user.flashcard_history[user.user.flashcard_history.length - 1].score
-    : 0
+  const flashcardElo =
+    user && user.user.flashcard_history && user.user.flashcard_history.length > 0
+      ? user.user.flashcard_history[user.user.flashcard_history.length - 1].score
+      : 0
 
-  const navBarStyle = smallWindow
-    ? {}
-    : { position: 'fixed', top: 0, width: '100%', zIndex: '100' }
+  const navBarStyle = smallWindow ? {} : { position: 'fixed', top: 0, width: '100%', zIndex: '100' }
 
-  const blackToWhiteFilter = 'invert(92%) sepia(94%) saturate(29%) hue-rotate(251deg) '
-    + 'brightness(108%) contrast(100%)'
+  const blackToWhiteFilter =
+    'invert(92%) sepia(94%) saturate(29%) hue-rotate(251deg) brightness(108%) contrast(100%)'
 
   if (!user) return null
 
   return (
-    <Headroom
-      disableInlineStyles={!smallWindow}
-      style={navBarStyle}
-    >
+    <Headroom disableInlineStyles={!smallWindow} style={navBarStyle}>
       <Navbar style={{ paddingLeft: '0.5em' }}>
         <div style={{ display: 'flex' }}>
           <Icon
@@ -60,37 +57,45 @@ export default function NavBar() {
             style={{ color: 'white' }}
             data-cy="hamburger"
           />
-          <Navbar.Brand
-            style={{ color: 'white', marginLeft: '0.5em', cursor: 'pointer' }}
-            onClick={() => history.push('/home')}
-          >
-
-            Revita{hiddenFeatures && <sup> &beta;</sup>}
-          </Navbar.Brand>
+          <Link to="/home">
+            <Navbar.Brand style={{ color: 'white', marginLeft: '0.5em', cursor: 'pointer' }}>
+              Revita{hiddenFeatures && <sup> &beta;</sup>}
+            </Navbar.Brand>
+          </Link>
         </div>
         {user && (
           <div>
-            <Navbar.Text style={{ color: 'white', marginRight: '1em', cursor: 'pointer' }} onClick={handleEloClick}>
-              {showStoryElo && <div><Icon name="star outline" style={{ margin: 0 }} /> {storyElo}</div>}
-              {showFlashcardElo
-                && (
-                  <div>
-                    <img
-                      src={images.flashcardIcon}
-                      alt="three cards"
-                      width="18px"
-                      style={{
-                        filter: blackToWhiteFilter,
-                        marginRight: '0.2em',
-                        marginBottom: '0.2em',
-                      }}
-                    />
-                    {flashcardElo}
-                  </div>
-                )}
+            <Navbar.Text
+              style={{ color: 'white', marginRight: '1em', cursor: 'pointer' }}
+              onClick={handleEloClick}
+            >
+              {showStoryElo && (
+                <div>
+                  <Icon name="star outline" style={{ margin: 0 }} /> {storyElo}
+                </div>
+              )}
+              {showFlashcardElo && (
+                <div>
+                  <img
+                    src={images.flashcardIcon}
+                    alt="three cards"
+                    width="18px"
+                    style={{
+                      filter: blackToWhiteFilter,
+                      marginRight: '0.2em',
+                      marginBottom: '0.2em',
+                    }}
+                  />
+                  {flashcardElo}
+                </div>
+              )}
             </Navbar.Text>
 
-            <Icon name="setting" style={{ color: 'white', cursor: 'pointer' }} onClick={handleSettingClick} />
+            <Icon
+              name="setting"
+              style={{ color: 'white', cursor: 'pointer' }}
+              onClick={handleSettingClick}
+            />
           </div>
         )}
       </Navbar>
