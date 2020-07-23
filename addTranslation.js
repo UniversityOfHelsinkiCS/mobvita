edb76ce981e0700339ce43ce477e20e6c4227d6c
atte <atte.h.1998@gmail.com>
2020-07-23 14:52:40 +0300

    Fix translation ke

--- addTranslation.js
+++ addTranslation.js
@@ -16,7 +16,6 @@ for (trPair of process.argv.slice(3)) {
 }
 
 
-
 const langs = ['fi', 'en', 'it', 'ru', 'sv']
 for (lang of langs) {
   const fileName = `./client/util/translations/revita/${lang}/LC_MESSAGES/messages.json`
