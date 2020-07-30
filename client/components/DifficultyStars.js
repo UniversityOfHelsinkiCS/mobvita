590498e6cfdc6ac18da4c792861cd5045abcfff4
Darake <adamson.gert@hotmail.com>
2020-07-30 19:27:50 +0300

    fetch recommended sites from user object

--- client/components/DifficultyStars.js
+++ client/components/DifficultyStars.js
@@ -3,6 +3,7 @@ import { Icon } from 'semantic-ui-react'
 
 export default ({ difficulty, size }) => {
   switch (difficulty) {
+    case 3:
     case 'high':
       return (
         <div>
@@ -11,6 +12,7 @@ export default ({ difficulty, size }) => {
           <Icon name="star outline" size={size} style={{ color: 'red' }} />
         </div>
       )
+    case 2:
     case 'average':
       return (
         <div>
@@ -18,6 +20,7 @@ export default ({ difficulty, size }) => {
           <Icon name="star outline" size={size} style={{ color: 'steelblue' }} />
         </div>
       )
+    case 1:
     case 'low':
       return (
         <div>
--- client/components/LibraryView/RecommendedSites.js
+++ client/components/LibraryView/RecommendedSites.js
@@ -1,20 +1,12 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Button } from 'react-bootstrap'
-import { Icon } from 'semantic-ui-react'
-
+import DifficultyStars from 'Components/DifficultyStars'
 
 export default function RecommendedSites() {
-  const suggestedSites = useSelector(({ metadata }) => metadata.suggestedSites)
+  const favouriteSites = useSelector(({ user }) => user.data.user.favourite_sites)
 
-  const icons = {
-    3: <div><Icon name="star outline" style={{ color: 'red' }} /><Icon name="star outline" style={{ color: 'red' }} /><Icon name="star outline" style={{ color: 'red' }} /></div>,
-    2: <div><Icon name="star outline" style={{ color: 'steelblue' }} /><Icon name="star outline" style={{ color: 'steelblue' }} /></div>,
-    1: <div><Icon name="star outline" style={{ color: 'forestgreen' }} /></div>,
-    0: <div><Icon name="star outline" style={{ color: 'black' }} /></div>,
-  }
-
-  const createRow = (site) => {
+  const createRow = site => {
     const { difficulty, name, url } = site
 
     return (
@@ -22,16 +14,12 @@ export default function RecommendedSites() {
         <Button variant="link" href={url} target="_blank">
           {name}
         </Button>
-        {difficulty >= 1 && difficulty <= 3 ? icons[difficulty] : icons[0]}
+        <DifficultyStars difficulty={difficulty} />
       </div>
     )
   }
 
-  const sitesList = suggestedSites.map(site => createRow(site))
+  const sitesList = favouriteSites.map(site => createRow(site))
 
-  return (
-    <div className="suggestedSources-container">
-      {sitesList}
-    </div>
-  )
+  return <div className="suggestedSources-container">{sitesList}</div>
 }
