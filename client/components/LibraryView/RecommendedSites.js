44a5179547add5f20d6252f3fd400a6dd15d318a
Darake <adamson.gert@hotmail.com>
2020-07-30 21:55:23 +0300

    make favourite sites deletable

--- client/components/LibraryView/RecommendedSites.js
+++ client/components/LibraryView/RecommendedSites.js
@@ -1,19 +1,35 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { Button } from 'react-bootstrap'
+import { Icon } from 'semantic-ui-react'
+import { updateFavouriteSites } from 'Utilities/redux/userReducer'
 import DifficultyStars from 'Components/DifficultyStars'
 
 export default function RecommendedSites() {
   const favouriteSites = useSelector(({ user }) => user.data.user.favourite_sites)
+
+  const dispatch = useDispatch()
+
+  const handleSiteDelete = url => {
+    dispatch(updateFavouriteSites(favouriteSites.filter(site => site.url !== url)))
+  }
 
   const createRow = site => {
     const { difficulty, name, url } = site
 
     return (
       <div className="suggestedSources-row" key={url}>
-        <Button variant="link" href={url} target="_blank">
-          {name || url}
-        </Button>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Icon
+            name="close"
+            color="grey"
+            style={{ cursor: 'pointer' }}
+            onClick={() => handleSiteDelete(url)}
+          />
+          <Button variant="link" style={{ padding: 0 }} href={url} target="_blank">
+            {name || url}
+          </Button>
+        </div>
         <DifficultyStars difficulty={difficulty} />
       </div>
     )
