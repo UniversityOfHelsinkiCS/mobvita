3feb03b276db68dd7ab823449738f2fe76c679c7
Darake <adamson.gert@hotmail.com>
2020-10-23 03:39:21 +0300

    dont show medal for user if not publishing progress in leaderboards

--- client/components/LeaderboardView/LeaderboardItem.js
+++ client/components/LeaderboardView/LeaderboardItem.js
@@ -1,15 +1,4 @@
 import React from 'react'
-import { images } from 'Utilities/common'
-
-const LeaderboardPosition = ({ position }) => {
-  if (position === 1)
-    return <img src={images.firstMedal} alt="first position medal" height="24px" />
-  if (position === 2)
-    return <img src={images.secondMedal} alt="second position medal" height="24px" />
-  if (position === 3)
-    return <img src={images.thirdMedal} alt="third position medal" height="24px" />
-  return position
-}
 
 const LeaderboardItem = ({ position, username, value, highlighted = false }) => (
   <div
@@ -18,7 +7,7 @@ const LeaderboardItem = ({ position, username, value, highlighted = false }) => 
   >
     <div className="flex" style={{ maxWidth: '75%' }}>
       <div className="center" style={{ width: '2.5rem', fontSize: '1.1rem' }}>
-        <LeaderboardPosition position={position} />
+        {position}
       </div>
       <div
         style={{
--- client/components/LeaderboardView/LeaderboardList.js
+++ client/components/LeaderboardView/LeaderboardList.js
@@ -1,19 +1,48 @@
 import React, { useMemo } from 'react'
 import { useSelector } from 'react-redux'
 import Spinner from 'Components/Spinner'
-import { useCurrentUser } from 'Utilities/common'
+import { useCurrentUser, images } from 'Utilities/common'
 import LeaderboardItem from './LeaderboardItem'
+
+const Medal = ({ position }) => (
+  <img src={images[`${position}Medal`]} alt={`${position} position medal`} height="24px" />
+)
 
 const LeaderboardList = ({ amountToShow }) => {
   const { leaderboard, user_rank: userPositionIndex } = useSelector(
     ({ leaderboard }) => leaderboard.data
   )
   const user = useCurrentUser()
+  const userParticipatingInCompetition = user.publish_progress
 
   const filteredLeaderboard = useMemo(
-    () => leaderboard?.filter(item => item.weekly_time_spent !== 0).splice(0, amountToShow),
+    () => leaderboard?.filter(item => item.weekly_time_spent !== 99).splice(0, amountToShow),
     [leaderboard]
   )
+
+  const isEligibleForMedal = (positionInList, positionToCheck) => {
+    const userInPositionToCheckOrHigher = userPositionIndex < positionToCheck
+    return (
+      (!userParticipatingInCompetition &&
+        userInPositionToCheckOrHigher &&
+        positionInList === positionToCheck + 1) ||
+      ((!userInPositionToCheckOrHigher || userParticipatingInCompetition) &&
+        positionInList === positionToCheck)
+    )
+  }
+
+  const getPositionToRender = (position, userIdForPosition) => {
+    const isCurrentUser = userIdForPosition === user.oid
+    if (
+      position > 4 ||
+      (userParticipatingInCompetition && position > 3) ||
+      (isCurrentUser && !userParticipatingInCompetition)
+    )
+      return position
+    if (isEligibleForMedal(position, 3)) return <Medal position="third" />
+    if (isEligibleForMedal(position, 2)) return <Medal position="second" />
+    return <Medal position="first" />
+  }
 
   if (!leaderboard) return <Spinner />
 
@@ -25,10 +54,10 @@ const LeaderboardList = ({ amountToShow }) => {
         ({ user_id: userId, username, weekly_time_spent: hoursPracticed }, index) => (
           <LeaderboardItem
             key={userId}
-            position={index + 1}
             username={username}
             value={`${hoursPracticed}h`}
             highlighted={userId === user.oid}
+            position={getPositionToRender(index + 1, userId)}
           />
         )
       )}
