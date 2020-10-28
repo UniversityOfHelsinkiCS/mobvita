cabfccdc95af2cdd30fc6b8e7848bf86c4ec0b3d
Darake <adamson.gert@hotmail.com>
2020-10-28 07:11:15 +0200

    remove time parameters from leaderboard fetch closes #772

--- client/util/redux/leaderboardReducer.js
+++ client/util/redux/leaderboardReducer.js
@@ -1,15 +1,10 @@
 import callBuilder from 'Utilities/apiConnection'
-import moment from 'moment'
 
 export const getLeaderboards = () => {
   const route = '/user/leaderboard'
-  const startTime = moment().format('YYYY-MM-DD')
-  const endTime = moment().add(7, 'd').format('YYYY-MM-DD')
   const query = {
     top_n: 25,
     sort_by: 'weekly_time_spent',
-    start_time: startTime,
-    end_time: endTime,
   }
   const prefix = 'GET_LEADERBOARDS'
   return callBuilder(route, prefix, 'get', null, query)
