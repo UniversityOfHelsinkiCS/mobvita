a84b04bd8dbb5d0f7638758b2254ae05ab062cba
Darake <adamson.gert@hotmail.com>
2020-11-27 09:54:41 +0200

    add Tatar keyboard layout & improve virtual keyboard

--- client/components/PracticeView/VirtualKeyboard.js
+++ client/components/PracticeView/VirtualKeyboard.js
@@ -5,7 +5,7 @@ import { Button } from 'react-bootstrap'
 import { Icon } from 'semantic-ui-react'
 import { setTouchedIds, setAnswers } from 'Utilities/redux/practiceReducer'
 import { learningLanguageSelector } from 'Utilities/common'
-import { keyboardLayouts } from './KeyboardLayouts'
+import { keyboardLayouts, keyboardDisplay } from './KeyboardLayouts'
 
 const VirtualKeyboard = () => {
   const learningLanguage = useSelector(learningLanguageSelector)
@@ -15,7 +15,8 @@ const VirtualKeyboard = () => {
   const [showKeyboard, setShowKeyboard] = useState(false)
   const [keyboardLayout, setKeyboardLayout] = useState(layoutsForLanguage[0].layout)
   const [layoutName, setLayoutName] = useState('default')
-  const [shift, setShift] = useState(false)
+  const [modifiers, setModifiers] = useState({ shift: false, capslock: false, ctrlAlt: false })
+  const [buttonTheme, setButtonTheme] = useState([])
 
   const { focusedWord, currentAnswers } = useSelector(({ practice }) => practice)
 
@@ -26,17 +27,53 @@ const VirtualKeyboard = () => {
     keyboard.setInput(currentAnswers[focusedWord.ID].users_answer)
   }, [focusedWord, keyboard, currentAnswers])
 
-  const handleKeyPress = (key) => {
-    const layout = layoutName === 'default' ? 'shift' : 'default'
+  const buildActiveModifersString = () =>
+    Object.entries(modifiers)
+      .reduce(
+        (tempString, [modifier, modifierActive]) =>
+          modifierActive ? `{${modifier}} ${tempString}` : tempString,
+        ''
+      )
+      .trim()
 
-    if (key === '{capslock}') {
-      setLayoutName(layout)
-    } else if (key === '{shift}') {
-      setShift(true)
-      setLayoutName(layout)
-    } else if (shift) {
-      setShift(false)
-      setLayoutName(layout)
+  const setCorrectButtonTheme = () => {
+    const tempTheme = [{ class: 'virtual-keyboard-ctrl-alt', buttons: '{ctrlAlt}' }]
+    const anyModifiersIsActive = Object.values(modifiers).some(modifier => modifier)
+    if (anyModifiersIsActive) {
+      tempTheme.push({
+        class: 'virtual-keyboard-active-key',
+        buttons: buildActiveModifersString(),
+      })
+    }
+    setButtonTheme(tempTheme)
+  }
+
+  const setCorrectLayout = () => {
+    const { shift, capslock, ctrlAlt } = modifiers
+    if (ctrlAlt && shift) setLayoutName('ctrlAltShift')
+    else if (ctrlAlt) setLayoutName('ctrlAlt')
+    else if (capslock && shift) setLayoutName('capsShift')
+    else if (capslock) setLayoutName('caps')
+    else if (shift) setLayoutName('shift')
+    else setLayoutName('default')
+  }
+
+  useEffect(() => {
+    setCorrectButtonTheme()
+    setCorrectLayout()
+  }, [modifiers])
+
+  const handleKeyPress = key => {
+    if (modifiers.shift) {
+      setModifiers({ ...modifiers, shift: false })
+    }
+
+    const trimmedKey = key.slice(1, -1)
+    if (Object.keys(modifiers).includes(trimmedKey)) {
+      setModifiers({
+        ...modifiers,
+        [trimmedKey]: !modifiers[trimmedKey],
+      })
     }
   }
 
@@ -67,13 +104,12 @@ const VirtualKeyboard = () => {
       />
       {showKeyboard && (
         <>
-          {layoutsForLanguage.length > 1
-            && layoutsForLanguage.map(layout => (
+          {layoutsForLanguage.length > 1 &&
+            layoutsForLanguage.map(layout => (
               <Button key={layout.name} onClick={() => setKeyboardLayout(layout.layout)}>
                 {layout.name}
               </Button>
-            ))
-          }
+            ))}
           <Keyboard
             keyboardRef={k => setKeyboard(k)}
             layout={keyboardLayout}
@@ -81,6 +117,8 @@ const VirtualKeyboard = () => {
             inputName={focusedWord.ID}
             onChange={handleAnswerChange}
             onKeyPress={handleKeyPress}
+            display={keyboardDisplay}
+            buttonTheme={buttonTheme}
           />
         </>
       )}
