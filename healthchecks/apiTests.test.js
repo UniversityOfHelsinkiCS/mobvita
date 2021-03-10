e9ca56a72b63c769ec9aed080bd6ccdf8d23dcb9
ilmarikyl <ilmari.kylliainen@helsinki.fi>
2021-03-10 13:53:51 +0200

    Add scheduled backend healthcheck

--- healthchecks/apiTests.test.js
+++ healthchecks/apiTests.test.js
@@ -5,6 +5,12 @@ const httpm = require('@actions/http-client')
 describe('basics', () => {
   beforeEach(() => {
     _http = new httpm.HttpClient('http-client-tests')
+
+    _http.requestOptions = {
+      headers: {
+        Authorization: process.env.REVITA_USER_TOKEN
+      }
+    }
   })
 
   afterEach(() => {})
@@ -14,20 +20,26 @@ describe('basics', () => {
     expect(http).toBeDefined()
   })
 
-  // responses from httpbin return something like:
-  // {
-  //     "args": {},
-  //     "headers": {
-  //       "Connection": "close",
-  //       "Host": "httpbin.org",
-  //       "User-Agent": "typed-test-client-tests"
-  //     },
-  //     "origin": "173.95.152.44",
-  //     "url": "https://httpbin.org/get"
-  //  }
-
   it('health check returns OK', async done => {
     let res = await _http.get('https://revita-old.cs.helsinki.fi/api/status')
+    expect(res.message.statusCode).toBe(200)
+    let body = await res.readBody()
+    let obj = JSON.parse(body)
+    expect(obj.message).toBe('OK')
+    done()
+  })
+
+  it('fetching next snippet works', async done => {
+    let res = await _http.get('https://revita-old.cs.helsinki.fi/api/stories/5c08248cff63453c5424000b/snippets/next')
+    expect(res.message.statusCode).toBe(200)
+    let body = await res.readBody()
+    let obj = JSON.parse(body)
+    expect(obj.message).toBe('OK')
+    done()
+  })
+
+  it('fetching flashcards works', async done => {
+    let res = await _http.get('https://revita-old.cs.helsinki.fi/api/flashcards/Russian/English?')
     expect(res.message.statusCode).toBe(200)
     let body = await res.readBody()
     let obj = JSON.parse(body)
