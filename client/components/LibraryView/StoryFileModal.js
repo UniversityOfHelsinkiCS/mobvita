498604abe8800bf40d531ccf3ab0fbfabe0d3fa9
ilmarikyl <ilmari.kylliainen@helsinki.fi>
2021-03-30 17:28:48 +0300

    Disable upload for filenames with non-Latin chars

--- client/components/LibraryView/StoryFileModal.js
+++ client/components/LibraryView/StoryFileModal.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Modal } from 'semantic-ui-react'
 import { Button, Spinner } from 'react-bootstrap'
-import { FormattedMessage, useIntl } from 'react-intl'
+import { FormattedMessage, FormattedHTMLMessage, useIntl } from 'react-intl'
 import { learningLanguageSelector } from 'Utilities/common'
 import { postStory, setCustomUpload } from 'Utilities/redux/uploadProgressReducer'
 import { updateLibrarySelect } from 'Utilities/redux/userReducer'
@@ -12,16 +12,23 @@ const StoryFileModal = ({ trigger }) => {
 
   const [file, setFile] = useState('')
   const [label, setLabel] = useState(intl.formatMessage({ id: 'choose-a-file' }))
+  const [filename, setfFilename] = useState('')
   const [showModel, setShowModel] = useState(false)
   const learningLanguage = useSelector(learningLanguageSelector)
   const { pending, storyId, progress } = useSelector(({ uploadProgress }) => uploadProgress)
 
   const dispatch = useDispatch()
 
+  const containsOnlyLatinCharacters = filename => {
+    if (filename) return /^[a-zA-Z0-9_\-\.]+$/.test(filename)
+    return true
+  }
+
   const onChange = e => {
     if (e.target.files[0]) {
       setFile(e.target.files[0])
       setLabel(e.target.files[0].name)
+      setfFilename(e.target.files[0].name)
     }
   }
 
@@ -43,7 +50,7 @@ const StoryFileModal = ({ trigger }) => {
 
   const storyUploading = pending || storyId
 
-  const submitDisabled = !file || storyUploading
+  const submitDisabled = !file || storyUploading || !containsOnlyLatinCharacters(filename)
 
   return (
     <Modal
@@ -58,9 +65,14 @@ const StoryFileModal = ({ trigger }) => {
         <FormattedMessage id="upload-stories" />
       </Modal.Header>
       <Modal.Content>
-        <span className="bold-italics">
-          <FormattedMessage id="please-submit-a-simple-text-file-containing-the-story-you-want-to-add-we-will-automatically-infer-th" />
+        <span className="bold">
+          <FormattedHTMLMessage id="file-upload-instructions" />
         </span>
+        {!containsOnlyLatinCharacters(filename) && (
+          <div style={{ color: 'red' }}>
+            <FormattedMessage id="check-for-non-latin-characters" />
+          </div>
+        )}
         <div className="space-evenly pt-lg">
           <input id="file" name="file" type="file" accept=".docx, .txt" onChange={onChange} />
           <label htmlFor="file">{label}</label>
