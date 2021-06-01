b3fac134e41a459a5cef09e0178e4d52505630d7
ilmarikyl <ilmari.kylliainen@helsinki.fi>
2021-06-01 10:49:50 +0300

    eslint

--- client/components/Concepts/ConceptToggles.js
+++ client/components/Concepts/ConceptToggles.js
@@ -5,9 +5,12 @@ import { useIntl } from 'react-intl'
 import { Checkbox } from 'semantic-ui-react'
 import { Spinner } from 'react-bootstrap'
 
-const ConceptToggles = (
-  { showTestConcepts, handleTestConceptToggle, showLevels, setShowLevels }
-) => {
+const ConceptToggles = ({
+  showTestConcepts,
+  handleTestConceptToggle,
+  showLevels,
+  setShowLevels,
+}) => {
   const { target } = useParams()
   const intl = useIntl()
   const { pending } = useSelector(({ groups }) => ({ pending: groups.testConceptsPending }))
@@ -21,30 +24,21 @@ const ConceptToggles = (
         onChange={() => setShowLevels(!showLevels)}
         className="concept-toggle"
       />
-      {target === 'groups'
-        && (
-          <div>
-            <Checkbox
-              toggle
-              label={intl.formatMessage({ id: 'show-test-settings' })}
-              checked={showTestConcepts}
-              onChange={handleTestConceptToggle}
-              className="concept-toggle"
-              disabled={pending}
-            />
-            {pending
-              && (
-                <Spinner
-                  animation="border"
-                  variant="primary"
-                  size="sm"
-                  style={{ marginLeft: '1em' }}
-                />
-              )
-            }
-          </div>
-        )
-      }
+      {target === 'groups' && (
+        <div>
+          <Checkbox
+            toggle
+            label={intl.formatMessage({ id: 'show-test-settings' })}
+            checked={showTestConcepts}
+            onChange={handleTestConceptToggle}
+            className="concept-toggle"
+            disabled={pending}
+          />
+          {pending && (
+            <Spinner animation="border" variant="primary" size="sm" style={{ marginLeft: '1em' }} />
+          )}
+        </div>
+      )}
     </div>
   )
 }
