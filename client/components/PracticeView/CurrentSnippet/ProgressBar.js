c52b1fa42fd22923dbf4572c10776432522a651e
ilmarikyl <ilmari.kylliainen@helsinki.fi>
2021-08-18 15:56:55 +0300

    tweak progress bar

--- client/components/PracticeView/CurrentSnippet/ProgressBar.js
+++ client/components/PracticeView/CurrentSnippet/ProgressBar.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Spinner from 'Components/Spinner'
 
 const ProgressBar = ({ snippetProgress, snippetsTotal, progress }) => {
   const getFontStyle = () => {
@@ -10,39 +9,39 @@ const ProgressBar = ({ snippetProgress, snippetsTotal, progress }) => {
   }
   return (
     <>
-      {!snippetsTotal ? (
-        <Spinner />
-      ) : (
-        <div
-          style={{
-            height: '1.5em',
-            textAlign: 'center',
-            borderRadius: '1rem',
-          }}
-          className="progress"
-        >
-          <span
-            data-cy="snippet-progress"
-            style={{
-              ...getFontStyle(),
-              marginTop: '0.75em',
-              position: 'absolute',
-              right: 0,
-              left: 0,
-            }}
-          >
-            {`${snippetProgress} / ${snippetsTotal}`}
-          </span>
-          <div
-            className="progress-bar progress-bar-striped bg-info"
-            style={{ width: `${progress * 100}%` }}
-            role="progressbar"
-            aria-valuenow={progress}
-            aria-valuemin="0"
-            aria-valuemax="100"
-          />
-        </div>
-      )}
+      <div
+        style={{
+          height: '1.5em',
+          textAlign: 'center',
+          borderRadius: '1rem',
+        }}
+        className="progress"
+      >
+        {!snippetsTotal ? null : (
+          <>
+            <span
+              data-cy="snippet-progress"
+              style={{
+                ...getFontStyle(),
+                marginTop: '0.75em',
+                position: 'absolute',
+                right: 0,
+                left: 0,
+              }}
+            >
+              {`${snippetProgress} / ${snippetsTotal}`}
+            </span>
+            <div
+              className="progress-bar progress-bar-striped bg-info"
+              style={{ width: `${progress * 100}%` }}
+              role="progressbar"
+              aria-valuenow={progress}
+              aria-valuemin="0"
+              aria-valuemax="100"
+            />
+          </>
+        )}
+      </div>
     </>
   )
 }
