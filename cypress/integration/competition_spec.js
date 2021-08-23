417d8a426252828374012fe2777add1caa16e736
ilmarikyl <ilmari.kylliainen@helsinki.fi>
2021-08-23 10:38:42 +0300

    disable competition tests temporarily

--- cypress/integration/competition_spec.js
+++ cypress/integration/competition_spec.js
@@ -1,37 +1,37 @@
-describe("competition mode", function () {
-  const competeURL = "http://localhost:8000/compete/5c080874ff6345361ec09dd8/"
+// describe("competition mode", function () {
+//   const competeURL = "http://localhost:8000/compete/5c080874ff6345361ec09dd8/"
 
-  this.beforeAll(function () {
-    cy.login()
-  })
+//   this.beforeAll(function () {
+//     cy.login()
+//   })
 
-  this.beforeEach(function () {
-    cy.loginExisting()
-    cy.visit(competeURL)
-  })
+//   this.beforeEach(function () {
+//     cy.loginExisting()
+//     cy.visit(competeURL)
+//   })
 
-  it("can be started", function () {
-    cy.get("[data-cy=opponent-bar]")
-    cy.get("[data-cy=player-bar]")
-  })
+//   it("can be started", function () {
+//     cy.get("[data-cy=opponent-bar]")
+//     cy.get("[data-cy=player-bar]")
+//   })
 
-  it("shows player's progress correctly", function () {
-    cy.get('[data-cy=next-snippet-ready] > span').click()
-    cy.get('[data-cy=progress-033]')
-    cy.get('[data-cy=next-snippet-ready] > span').click()
-    cy.get('[data-cy=progress-067]')
-    cy.get('[data-cy=next-snippet-ready] > span').click()
-    cy.get('[data-cy=progress-100]')
-  })
+//   it("shows player's progress correctly", function () {
+//     cy.get('[data-cy=next-snippet-ready] > span').click()
+//     cy.get('[data-cy=progress-033]')
+//     cy.get('[data-cy=next-snippet-ready] > span').click()
+//     cy.get('[data-cy=progress-067]')
+//     cy.get('[data-cy=next-snippet-ready] > span').click()
+//     cy.get('[data-cy=progress-100]')
+//   })
 
-  it("shows end modal when competition has ended", function () {
-    cy.get('[data-cy=next-snippet-ready] > span').click()
-    cy.get('[data-cy=next-snippet-ready] > span').click()
-    cy.get('[data-cy=next-snippet-ready] > span').click()
-    cy.get('[data-cy=competition-end-modal]')
-  })
+//   it("shows end modal when competition has ended", function () {
+//     cy.get('[data-cy=next-snippet-ready] > span').click()
+//     cy.get('[data-cy=next-snippet-ready] > span').click()
+//     cy.get('[data-cy=next-snippet-ready] > span').click()
+//     cy.get('[data-cy=competition-end-modal]')
+//   })
 
-  this.afterAll(function () {
-    cy.cleanUsers()
-  })
-})
+//   this.afterAll(function () {
+//     cy.cleanUsers()
+//   })
+// })
