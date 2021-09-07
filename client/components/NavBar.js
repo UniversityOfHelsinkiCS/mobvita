be23a7f66a2608236f47ce7730cd606f5b1aefde
ilmarikyl <ilmari.kylliainen@helsinki.fi>
2021-09-07 15:12:36 +0300

    change UI colors like they were before

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -100,8 +100,8 @@ export default function NavBar() {
                   name="home"
                   size="large"
                   style={{
-                    // color: 'white',
-                    color: 'rgb(0,0,190)',
+                    color: 'white',
+                    // color: 'rgb(0,0,190)',
                     cursor: 'pointer',
                   }}
                 />
@@ -112,11 +112,12 @@ export default function NavBar() {
                     src={images.navbarLogo}
                     alt="revita logo"
                     width="70"
-                    style={{
-                      filter: 'brightness(50%) sepia(100) saturate(100) hue-rotate(230deg)',
-                    }}
+                    // style={{
+                    //   filter: 'brightness(50%) sepia(100) saturate(100) hue-rotate(230deg)',
+                    // }}
                   />
-                  {hiddenFeatures && <sup style={{ color: 'rgb(0,0,190)' }}> &beta;</sup>}
+                  {/* {hiddenFeatures && <sup style={{ color: 'rgb(0,0,190)' }}> &beta;</sup>} */}
+                  {hiddenFeatures && <sup> &beta;</sup>}
                 </Navbar.Brand>
               </Link>
 
