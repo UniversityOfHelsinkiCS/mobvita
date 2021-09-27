6406b7489a6d13fd45d98faf4112241b0373bccd
ilmarikyl <ilmari.kylliainen@helsinki.fi>
2021-09-27 15:07:51 +0300

    rearrange profile settings page

--- client/components/Profile/Settings.js
+++ client/components/Profile/Settings.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { FormattedMessage, useIntl } from 'react-intl'
 import { Checkbox, Radio, Dropdown } from 'semantic-ui-react'
-import { localeNameToCode, localeOptions } from 'Utilities/common'
+import { localeNameToCode, localeOptions, hiddenFeatures } from 'Utilities/common'
 import { Button } from 'react-bootstrap'
 import {
   updateLocale,
@@ -62,7 +62,7 @@ const Settings = () => {
 
   return (
     <div className="const ps-nm">
-      <h2 className="header-2 pb-sm pt-nm">
+      <h2 className="profile-page-setting-header">
         <FormattedMessage id="interface-language" />
       </h2>
       <Dropdown
@@ -76,18 +76,102 @@ const Settings = () => {
         style={{ color: '#777', marginTop: marginTopButton, width: '30%' }}
       />
       <hr />
-      <h2 className="header-2 pb-sm pt-nm">
+      <h2 className="profile-page-setting-header">
+        <FormattedMessage id="Privacy" />
+      </h2>
+      <SettingToggle
+        translationId="Show my username in leaderboards"
+        checked={user.publish_progress}
+        onChange={() => dispatch(updatePublishProgress(!user.publish_progress))}
+        disabled={pending}
+      />
+      <hr />
+      <h2 className="profile-page-setting-header">
         <FormattedMessage id="select-types-of-exercises-to-practice" />
       </h2>
-      <br />
-      <LearningSettingsModal
-        trigger={
-          <Button variant="primary" size="lg">
-            <FormattedMessage id="learning-settings" />
-          </Button>
-        }
+      <div className="flex-col align-start gap-row-sm">
+        <SettingToggle
+          translationId="multiple-choice"
+          checked={user.multi_choice}
+          onChange={() => dispatch(updateMultiChoice(!user.multi_choice))}
+          disabled={pending}
+        />
+        <span style={{ display: 'none', color: 'gray' }}>
+          <i>Temporarily unavailable due to technical problem</i>
+        </span>
+        <br />
+        <SettingToggle
+          translationId="multiple-chances-when-practicing"
+          checked={user.second_try}
+          onChange={() => dispatch(updateSecondTry(!user.second_try))}
+          disabled={pending}
+        />
+
+        {hiddenFeatures && (
+          <div>
+            <hr />
+            <span className="pb-sm">
+              <FormattedMessage id="participle-exercise" /> (staging only):
+            </span>
+            <div className="gap-col-nm" style={{ marginTop: '1em' }}>
+              <Radio
+                label={intl.formatMessage({ id: 'participle-base-exer' })}
+                name="part_exer"
+                value="participle"
+                checked={user.part_exer === 'participle'}
+                onChange={() => dispatch(updateParticipleExer('participle'))}
+              />
+              <Radio
+                label={intl.formatMessage({ id: 'verb-base-exer' })}
+                name="part_exer"
+                value="verb"
+                checked={user.part_exer === 'verb'}
+                onChange={() => dispatch(updateParticipleExer('verb'))}
+              />
+            </div>
+          </div>
+        )}
+      </div>
+
+      <hr />
+
+      <h2 className="profile-page-setting-header">
+        <FormattedMessage id="Flashcards" />
+      </h2>
+      <label htmlFor="flashcard-amount" style={{ paddingRight: '0.5rem' }}>
+        <FormattedMessage id="how-many-cards-per-practice-session" />
+        :&nbsp;&nbsp;
+      </label>
+      <Dropdown
+        id="flashcard-amount"
+        value={user.flashcard_num}
+        options={deckSizeOptions}
+        onChange={(e, data) => dispatch(updateNumberOfFlashcards(Number(data.value)))}
+        disabled={pending}
       />
-      <br />
+      <hr />
+      <h2 className="profile-page-setting-header">
+        <FormattedMessage id="Audio settings" />
+      </h2>
+      <span className="pb-sm">
+        <FormattedMessage id="Pronounce clicked words" />:
+      </span>
+      <div className="gap-col-nm">
+        <Radio
+          label={intl.formatMessage({ id: 'Always' })}
+          name="autoSpeak"
+          value="always"
+          checked={user.auto_speak === 'always'}
+          onChange={() => dispatch(updateAutoSpeak('always'))}
+        />
+        <Radio
+          label={intl.formatMessage({ id: 'Only on demand' })}
+          name="autoSpeak"
+          value="demand"
+          checked={user.auto_speak === 'demand'}
+          onChange={() => dispatch(updateAutoSpeak('demand'))}
+        />
+      </div>
       <br />
       <span className="pb-sm">
         <FormattedMessage id="type-the-word-you-hear" />:
@@ -115,102 +199,18 @@ const Settings = () => {
           onChange={() => dispatch(updateAudioTask('none'))}
         />
       </div>
-      <br />
-      <span className="pb-sm">
-        <FormattedMessage id="participle-exercise" />:
-      </span>
-      <div className="gap-col-nm">
-        <Radio
-          label={intl.formatMessage({ id: 'participle-base-exer' })}
-          name="part_exer"
-          value="participle"
-          checked={user.part_exer === 'participle'}
-          onChange={() => dispatch(updateParticipleExer('participle'))}
-        />
-        <Radio
-          label={intl.formatMessage({ id: 'verb-base-exer' })}
-          name="part_exer"
-          value="verb"
-          checked={user.part_exer === 'verb'}
-          onChange={() => dispatch(updateParticipleExer('verb'))}
-        />
-        {/* <Radio
-          label={intl.formatMessage({ id: 'no-listen-exer' })}
-          name="part_exer"
-          value="auto"
-          checked={user.part_exer === 'auto'}
-          onChange={() => dispatch(updateParticipleExer('auto'))}
-        /> */}
-      </div>
-      <br />
-      <div className="flex-col align-start gap-row-sm">
-        <SettingToggle
-          translationId="multiple-choice"
-          checked={user.multi_choice}
-          onChange={() => dispatch(updateMultiChoice(!user.multi_choice))}
-          disabled={pending}
-        />
-        <span style={{ display: 'none', color: 'gray' }}>
-          <i>Temporarily unavailable due to technical problem</i>
-        </span>
-        <br />
-        <SettingToggle
-          translationId="multiple-chances-when-practicing"
-          checked={user.second_try}
-          onChange={() => dispatch(updateSecondTry(!user.second_try))}
-          disabled={pending}
-        />
-      </div>
+
       <hr />
-      <h2 className="header-2 pb-sm pt-nm">
-        <FormattedMessage id="Audio settings" />
+
+      <h2 className="profile-page-setting-header">
+        <FormattedMessage id="learning-settings" />
       </h2>
-      <br />
-      <span className="pb-sm">
-        <FormattedMessage id="Pronounce clicked words" />:
-      </span>
-      <div className="gap-col-nm">
-        <Radio
-          label={intl.formatMessage({ id: 'Always' })}
-          name="autoSpeak"
-          value="always"
-          checked={user.auto_speak === 'always'}
-          onChange={() => dispatch(updateAutoSpeak('always'))}
-        />
-        <Radio
-          label={intl.formatMessage({ id: 'Only on demand' })}
-          name="autoSpeak"
-          value="demand"
-          checked={user.auto_speak === 'demand'}
-          onChange={() => dispatch(updateAutoSpeak('demand'))}
-        />
-      </div>
-      <hr />
-      <h2 className="header-2 pb-sm pt-nm">
-        <FormattedMessage id="Flashcards" />
-      </h2>
-      <br />
-      <label htmlFor="flashcard-amount" style={{ paddingRight: '0.5rem' }}>
-        <FormattedMessage id="how-many-cards-per-practice-session" />
-        :&nbsp;&nbsp;
-      </label>
-      <Dropdown
-        id="flashcard-amount"
-        value={user.flashcard_num}
-        options={deckSizeOptions}
-        onChange={(e, data) => dispatch(updateNumberOfFlashcards(Number(data.value)))}
-        disabled={pending}
-      />
-      <hr />
-      <h2 className="header-2 pb-sm pt-nm">
-        <FormattedMessage id="Privacy" />
-      </h2>
-      <br />
-      <SettingToggle
-        translationId="Show my username in leaderboards"
-        checked={user.publish_progress}
-        onChange={() => dispatch(updatePublishProgress(!user.publish_progress))}
-        disabled={pending}
+      <LearningSettingsModal
+        trigger={
+          <Button variant="primary" size="lg">
+            <FormattedMessage id="edit-learning-settings" />
+          </Button>
+        }
       />
     </div>
   )
