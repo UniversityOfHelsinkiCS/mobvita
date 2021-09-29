d6e2d0b00e1dca7e648ed6e541b98ecb0e9490b8
ilmarikyl <ilmari.kylliainen@helsinki.fi>
2021-09-29 15:53:32 +0300

    tweak adap test start page

--- client/components/Tests/AdaptiveTest/index.js
+++ client/components/Tests/AdaptiveTest/index.js
@@ -3,9 +3,8 @@ import { Button } from 'react-bootstrap'
 import { FormattedMessage } from 'react-intl'
 import { useDispatch, useSelector } from 'react-redux'
 import { InitAdaptiveTest, resetTests } from 'Utilities/redux/testReducer'
-
 import { useLearningLanguage } from 'Utilities/common'
-import useWindowDimension from 'Utilities/windowDimensions'
+// import useWindowDimension from 'Utilities/windowDimensions'
 import Spinner from 'Components/Spinner'
 import ReportButton from 'Components/ReportButton'
 import TestView from './AdaptiveTest'
@@ -33,7 +32,7 @@ const AdaptiveTestView = () => {
 
   return (
     <div className="cont-tall cont flex-col auto gap-row-sm">
-      <div className="grow ps-nm flex-col gap-row-sm">
+      <div className="grow ps-nm flex-col gap-row-sm mt-md">
         {!adaptiveTestSessionId && (
           <div className="pl-nm pt-nm">
             <Button onClick={startTest} data-cy="start-test">
@@ -43,6 +42,9 @@ const AdaptiveTestView = () => {
         )}
         {/* {report && <TestReport />} */}
         {adaptiveTestSessionId && <TestView />}
+        <div>
+          <hr className="my-2" />
+        </div>
         <ReportButton extraClass="align-self-end mb-sm" />
       </div>
     </div>
