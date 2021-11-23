fbd24dd98f486b469d38a13519aceb58f3d7d779
ilmarikyl <ilmari.kylliainen@helsinki.fi>
2021-11-23 20:03:58 +0200

    eslint

--- client/components/ConnectedIntlProvider.js
+++ client/components/ConnectedIntlProvider.js
@@ -6,13 +6,13 @@ import { localeNameToCode } from '../util/common'
 const mapStateToProps = ({ locale, user }) => {
   let actualLocale = locale
 
-  if (user.data && user.data.user && user.data.user.interfaceLanguage) { // If user has logged in, use locale from user object, else use value from localeReducer
+  if (user.data && user.data.user && user.data.user.interfaceLanguage) {
+    // If user has logged in, use locale from user object, else use value from localeReducer
     actualLocale = localeNameToCode(user.data.user.interfaceLanguage)
   }
 
-  const messages = actualLocale === 'en'
-    ? translations.en
-    : { ...translations.en, ...translations[actualLocale] }
+  const messages =
+    actualLocale === 'en' ? translations.en : { ...translations.en, ...translations[actualLocale] }
 
   return { key: actualLocale, locale: actualLocale, messages }
 }
