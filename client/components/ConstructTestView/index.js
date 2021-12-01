09cf7ef8282f1fd56479fbc641166b289d61e238
ilmarikyl <ilmari.kylliainen@helsinki.fi>
2021-12-01 14:39:07 +0200

    improvements to construction test feature

--- client/components/ConstructTestView/index.js
+++ client/components/ConstructTestView/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { Segment, Input, Divider } from 'semantic-ui-react'
+import { Segment, Input, Divider, Table, Checkbox } from 'semantic-ui-react'
 import { learningLanguageSelector } from 'Utilities/common'
 import { testConstruction, resetConstructionResults } from 'Utilities/redux/constructionTestReducer'
 import { Spinner } from 'react-bootstrap'
@@ -10,6 +10,7 @@ const ConstructTestView = () => {
   const learningLanguage = useSelector(learningLanguageSelector)
 
   const [text, setText] = useState('')
+  const [showAnalyses, setShowAnalyses] = useState(false)
 
   const { patternResults, pending } = useSelector(({ constructionTest }) => constructionTest)
 
@@ -33,18 +34,17 @@ const ConstructTestView = () => {
     }
   }
 
+  // console.log(patternResults)
+
   const syntaxHighlight = json => {
-    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
-
-    return json.replace(/(&lt;c[0-9]&gt;.*?&lt;\/c[0-9]&gt;)/g, match => {
+    return json.replace(/(<c[0-9]>.*?<\/c[0-9]>)/g, match => {
       let color = ''
-
-      if (/(&lt;c1&gt;.*?&lt;\/c1&gt;)/.test(match)) color = 'blue'
-      if (/(&lt;c2&gt;.*?&lt;\/c2&gt;)/.test(match)) color = 'red'
-      if (/(&lt;c3&gt;.*?&lt;\/c3&gt;)/.test(match)) color = 'green'
+      if (/(<c1>.*?<\/c1>)/.test(match)) color = 'blue'
+      if (/(<c2>.*?<\/c2>)/.test(match)) color = 'red'
+      if (/(<c3>.*?<\/c3>)/.test(match)) color = 'green'
 
       return `<span style="color: ${color}">${match.replace(
-        /&lt;c[0-9]&gt;(.*?)&lt;\/c[0-9]&gt;/g,
+        /<c[0-9]>(.*?)<\/c[0-9]>/g,
         '$1'
       )}</span>`
     })
@@ -55,42 +55,107 @@ const ConstructTestView = () => {
       <div className="justify-center">
         <div className="cont">
           <Segment>
-            <div style={{ maxWidth: '550px' }}>
-              <Input
-                action={{
-                  content: pending ? (
-                    <Spinner animation="border" variant="primary" size="sm" />
-                  ) : (
-                    'Send'
-                  ),
-                  onClick: handleClick,
-                }}
-                placeholder="Enter a sentence..."
-                size="large"
-                fluid
-                onChange={e => setText(e.target.value)}
-                onKeyDown={handleKeyDown}
-              />
+            <div className="space-between align-center">
+              <div style={{ width: '550px' }}>
+                <Input
+                  action={{
+                    content: pending ? (
+                      <Spinner animation="border" variant="primary" size="sm" />
+                    ) : (
+                      'Send'
+                    ),
+                    onClick: handleClick,
+                  }}
+                  placeholder="Enter a sentence..."
+                  size="large"
+                  fluid
+                  onChange={e => setText(e.target.value)}
+                  onKeyDown={handleKeyDown}
+                />
+              </div>
+              {patternResults && (
+                <Checkbox
+                  toggle
+                  label="Show analyses"
+                  checked={showAnalyses}
+                  onChange={() => setShowAnalyses(!showAnalyses)}
+                />
+              )}
             </div>
 
             {patternResults && (
               <>
                 <Divider />
-                <div
-                  style={{
-                    borderRadius: '7px',
-                    padding: '1em',
-                    background: '#f5f5f5',
-                    minHeight: '500px',
-                  }}
-                >
-                  <div style={{ overflow: 'auto', maxHeight: '1100px' }}>
-                    <pre
-                      dangerouslySetInnerHTML={{
-                        __html: syntaxHighlight(JSON.stringify(patternResults, null, 2)),
+
+                <div style={{ marginTop: '1.5em' }}>
+                  {patternResults.map(resultObj => (
+                    <div
+                      style={{
+                        borderRadius: '7px',
+                        padding: '1em',
+                        background: '#f5f5f5',
+                        marginBottom: '.75em',
                       }}
-                    />
-                  </div>
+                    >
+                      <div className="bold" style={{ marginLeft: '.5em', fontSize: '1.2em' }}>
+                        {patternResults[0].message}
+                      </div>
+
+                      <Table size="small" celled>
+                        <Table.Body>
+                          <Table.Row>
+                            <Table.Cell width={3}>
+                              <b>Sentence</b>
+                            </Table.Cell>
+                            <Table.Cell>
+                              <div
+                                dangerouslySetInnerHTML={{
+                                  __html: syntaxHighlight(resultObj.sentence),
+                                }}
+                              />
+                            </Table.Cell>
+                          </Table.Row>
+                        </Table.Body>
+                      </Table>
+                      {/* {console.log(resultObj.table)} */}
+                      <Table size="small" celled>
+                        <Table.Body>
+                          {Object.keys(resultObj.table).map(key => (
+                            <Table.Row>
+                              <Table.Cell width={3}>
+                                <b>{key}</b>
+                              </Table.Cell>
+                              <Table.Cell style={{ color: 'green' }}>
+                                {resultObj.table[key]}
+                              </Table.Cell>
+                            </Table.Row>
+                          ))}
+
+                        </Table.Body>
+                      </Table>
+                      <div style={{ margin: '1em .5em 2em .5em', whiteSpace: 'pre-line' }}>
+                        {resultObj.matches}
+                      </div>
+                      {showAnalyses && (
+                        <>
+                          <Divider />
+                          <div className="bold" style={{ marginLeft: '.5em' }}>
+                            Analyses:
+                          </div>
+                          <div style={{ overflow: 'auto', maxHeight: '500px', marginLeft: '.5em' }}>
+                            <pre
+                              dangerouslySetInnerHTML={{
+                                __html: syntaxHighlight(
+                                  JSON.stringify(resultObj.analysis, null, 2)
+                                ),
+                              }}
+                            />
+                          </div>
+                        </>
+                      )}
+                    </div>
+                  ))}
+
                 </div>
               </>
             )}
