f83149ccbed0b22966bb55bf67ff482e6a97eabd
ilmarikyl <ilmari.kylliainen@helsinki.fi>
2022-01-04 11:52:31 +0200

    start a new flashcard deck with right arrow or enter

--- client/components/Flashcards/Practice/FlashcardEndView.js
+++ client/components/Flashcards/Practice/FlashcardEndView.js
@@ -1,25 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { FormattedMessage } from 'react-intl'
 import { Button } from 'react-bootstrap'
 
-const FlashcardEndView = ({ handleNewDeck }) => (
-  <div className="flashcard justify-center">
-    <div>
-      <p style={{ fontWeight: '500', fontSize: '1.2em', padding: '1em' }}>
-        <FormattedMessage id="well-done-click-next-card-to-play-another-set-of-cards" />
-      </p>
-      <div className="flashcard-input" style={{ flex: 0 }}>
-        <Button
-          className="flashcard-button"
-          block
-          variant="outline-primary"
-          onClick={() => handleNewDeck()}
-        >
-          <FormattedMessage id="next-card" />
-        </Button>
+const FlashcardEndView = ({ handleNewDeck }) => {
+  // A bit hacky way to move to next deck with right arrow or enter
+  const useKeyPress = targetKey => {
+    const [keyPressed, setKeyPressed] = useState(false)
+    function downHandler({ key }) {
+      if (key === targetKey) setKeyPressed(true)
+    }
+
+    const upHandler = ({ key }) => {
+      if (key === targetKey) setKeyPressed(false)
+    }
+
+    useEffect(() => {
+      setTimeout(() => {
+        window.addEventListener('keydown', downHandler)
+        window.addEventListener('keyup', upHandler)
+      }, 2000)
+
+      return () => {
+        window.removeEventListener('keydown', downHandler)
+        window.removeEventListener('keyup', upHandler)
+      }
+    }, [])
+
+    return keyPressed
+  }
+
+  const RightArrowPress = useKeyPress('ArrowRight')
+  const EnterPress = useKeyPress('Enter')
+
+  if (RightArrowPress || EnterPress) {
+    handleNewDeck()
+  }
+
+  return (
+    <div className="flashcard justify-center">
+      <div>
+        <p style={{ fontWeight: '500', fontSize: '1.2em', padding: '1em' }}>
+          <FormattedMessage id="well-done-click-next-card-to-play-another-set-of-cards" />
+        </p>
+        <div className="flashcard-input" style={{ flex: 0 }}>
+          <Button
+            className="flashcard-button"
+            block
+            variant="outline-primary"
+            onClick={() => handleNewDeck()}
+          >
+            <FormattedMessage id="next-card" />
+          </Button>
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default FlashcardEndView
