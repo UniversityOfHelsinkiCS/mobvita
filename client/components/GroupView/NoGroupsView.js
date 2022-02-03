7c757226829a1751e5eac307e4baeffa28f2d8f4
Valtteri Andström <valtteri.andstrom@gmail.com>
2022-02-03 14:30:13 +0200

    Anonymous users cant create or join groups

--- client/components/GroupView/NoGroupsView.js
+++ client/components/GroupView/NoGroupsView.js
@@ -1,36 +1,62 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
 import { Button } from 'react-bootstrap'
+import { useCurrentUser } from 'Utilities/common'
+import { useHistory } from 'react-router-dom'
 import GroupActionModal from './GroupActionModal'
 
 const NoGroupsView = ({ role }) => {
+  const user = useCurrentUser()
+  const isAnonymousUser = user.email === 'anonymous_email'
+  const history = useHistory()
+
   return (
     <div className="group-container nogroups" data-cy="no-groups-view">
       <h2 id="title">
         <FormattedMessage id={role === 'student' ? 'Groups-for-students' : 'Groups-for-teachers'} />
       </h2>
 
-      <GroupActionModal
-        role={role}
-        trigger={
+      {isAnonymousUser ? (
+        <span className="additional-info">
+          <FormattedMessage id="groups-for-only-registered-users" />
           <div>
             <Button
+              className="mt-nm"
               variant="primary"
               size="lg"
-              data-cy={role === 'student' ? 'join-group-button' : 'create-group-button'}
+              onClick={() => history.push('/register')}
             >
-              <FormattedMessage id={role === 'student' ? 'join-a-group' : 'create-new-group'} />
+              <FormattedMessage id="Register" />
             </Button>
           </div>
-        }
-      />
-      <br />
-      <span className="additional-info">
-        <FormattedMessage id={role === 'student' ? 'join-group-message' : 'create-group-message'} />
-        <br />
-        <br />
-        <FormattedMessage id="cant-find-group" />
-      </span>
+        </span>
+      ) : (
+        <>
+          <GroupActionModal
+            role={role}
+            trigger={
+              <div>
+                <Button
+                  variant="primary"
+                  size="lg"
+                  data-cy={role === 'student' ? 'join-group-button' : 'create-group-button'}
+                >
+                  <FormattedMessage id={role === 'student' ? 'join-a-group' : 'create-new-group'} />
+                </Button>
+              </div>
+            }
+          />
+          <br />
+          <span className="additional-info">
+            <FormattedMessage
+              id={role === 'student' ? 'join-group-message' : 'create-group-message'}
+            />
+            <br />
+            <br />
+            <FormattedMessage id="cant-find-group" />
+          </span>
+        </>
+      )}
     </div>
   )
 }
