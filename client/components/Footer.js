0a63b682813aadd76bfa014cc284883fc7babe54
Valtteri Andström <valtteri.andstrom@gmail.com>
2022-02-15 13:37:52 +0200

    update footer

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -27,7 +27,7 @@ const Footer = () => {
             </div>
           </>
         )}
-        <div className="footer-item">© University of Helsinki 2020–2021</div>
+        <div className="footer-item">© University of Helsinki 2021–2022</div>
 
         {showResonsiveVoiceMention && (
           <div className="footer-item">
