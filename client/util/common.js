f57b982ec1074c647b771d6890db73ac78aaf20c
Katinskaia Anisia <anisia.katinskaia@helsinki.fi>
2022-03-03 13:16:07 +0200

    Change lang name

--- client/util/common.js
+++ client/util/common.js
@@ -11,7 +11,7 @@ import flagKomizyrian from 'Assets/images/flags/flag_komi-zyrian.png'
 import flagSakha from 'Assets/images/flags/flag_sakha.png'
 import flagTatar from 'Assets/images/flags/flag_tatar.png'
 import flagUdmurt from 'Assets/images/flags/flag_udmurt.png'
-import flagUdmurtnew from 'Assets/images/flags/flag_udmurt.png'
+import flagUdmurtarch from 'Assets/images/flags/flag_udmurt.png'
 import flagMeadowmari from 'Assets/images/flags/flag_meadow-mari.png'
 import flagNorthsaami from 'Assets/images/flags/flag_north-saami.png'
 import flagCatalan from 'Assets/images/flags/flag_catalan.png'
@@ -76,7 +76,7 @@ export const images = {
   revitaLogoTransparent,
   flagFinnish,
   flagUdmurt,
-  flagUdmurtnew,
+  flagUdmurtarch,
   flagErzya,
   flagKomizyrian,
   flagMeadowmari,
@@ -182,7 +182,7 @@ export const supportedLearningLanguages = {
     'chinese',
     'turkish',
   ].sort((a, b) => a.localeCompare(b)),
-  minor: ['erzya', 'komi-zyrian', 'meadow-mari', 'north-saami', 'sakha', 'tatar', 'udmurt', 'udmurt-new'].sort(
+  minor: ['erzya', 'komi-zyrian', 'meadow-mari', 'north-saami', 'sakha', 'tatar', 'udmurt', 'udmurt-arch'].sort(
     (a, b) => a.localeCompare(b)
   ),
   experimental: ['syriac'],
@@ -209,7 +209,7 @@ export const learningLanguageLocaleCodes = {
   Turkish: 'tr',
   Syriac: 'syc',
   Chinese: 'zh',
-  'Udmurt-New': 'udm'
+  'Udmurt-Arch': 'udm'
 }
 
 export const betaLanguages = [
@@ -231,7 +231,7 @@ export const betaLanguages = [
   'tatar',
   'turkish',
   'udmurt',
-  'udmurt-new'
+  'udmurt-arch'
 ]
 
 export const exerciseMaskedLanguages = ['Chinese']
@@ -581,7 +581,7 @@ export const translatableLanguages = {
   'Komi-Zyrian': ['Russian'],
   'Meadow-Mari': ['Russian'],
   Udmurt: ['Russian'],
-  'Udmurt-New': ['Russian'],
+  'Udmurt-Arch': ['Russian'],
   Sakha: ['Russian', 'English'],
   'North-Saami': ['Finnish', 'Norwegian'],
   Erzya: ['Russian'],
