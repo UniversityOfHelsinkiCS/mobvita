fd6442a0c6189b5db6a0a88e4f24258c45b32a43
Valtteri Andström <valtteri.andstrom@gmail.com>
2022-03-18 20:30:05 +0200

    Change the order of toggles in vocabulary chart

--- client/components/VocabularyView/VocabularyGraph.js
+++ client/components/VocabularyView/VocabularyGraph.js
@@ -126,21 +126,6 @@ const VocabularyGraph = ({ vocabularyData }) => {
         linkedTo: 'Total',
       },
       {
-        name: intl.formatMessage({ id: 'vocabulary-visit' }),
-        id: 'Visit',
-        data: visit.now,
-        visible: false,
-      },
-      {
-        name: `${intl.formatMessage({ id: 'vocabulary-visit' })} ${intl.formatMessage({
-          id: 'vocabulary-follow-statistic-before',
-        })}`,
-        id: 'Visit (before)',
-        data: visit[Object.keys(visit).filter(key => key !== 'now')[0]],
-        linkedTo: 'Visit',
-        visible: false,
-      },
-      {
         name: intl.formatMessage({ id: 'vocabulary-seen' }),
         id: 'Seen',
         data: seen.now,
@@ -153,6 +138,21 @@ const VocabularyGraph = ({ vocabularyData }) => {
         id: 'Seen (before)',
         data: seen[Object.keys(seen).filter(key => key !== 'now')[0]],
         linkedTo: 'Seen',
+        visible: false,
+      },
+      {
+        name: intl.formatMessage({ id: 'vocabulary-visit' }),
+        id: 'Visit',
+        data: visit.now,
+        visible: false,
+      },
+      {
+        name: `${intl.formatMessage({ id: 'vocabulary-visit' })} ${intl.formatMessage({
+          id: 'vocabulary-follow-statistic-before',
+        })}`,
+        id: 'Visit (before)',
+        data: visit[Object.keys(visit).filter(key => key !== 'now')[0]],
+        linkedTo: 'Visit',
         visible: false,
       },
       {
