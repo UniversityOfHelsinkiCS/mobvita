158d5cf2f7e8df92c80afa00b21d1dd9785e5e78
Valtteri Andström <valtteri.andstrom@gmail.com>
2022-03-30 18:29:48 +0300

    update localizations

--- client/components/Encouragements/WelcomeBackEncouragementModal.js
+++ client/components/Encouragements/WelcomeBackEncouragementModal.js
@@ -88,11 +88,11 @@ const WelcomeBackEncouragementModal = ({
                       id="leaderboard-ranking-encouragement"
                       values={{ userRanking }}
                     />
-                    <div>
-                      <Link to="/leaderboard">
-                        <FormattedMessage id="leaderboard-link-encouragement" />
-                      </Link>
-                    </div>
+                    &nbsp;
+                    <Link to="/leaderboard">
+                      <FormattedMessage id="leaderboard-link-encouragement" />
+                    </Link>
+                    !
                   </div>
                   <div>
                     <FormattedMessage id="practice-makes-perfect" />
