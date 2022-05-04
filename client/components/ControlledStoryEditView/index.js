dda9427a16ce36ba6184ed19837bfb7558a39814
Valtteri Andström <valtteri.andstrom@gmail.com>
2022-05-04 15:49:05 +0300

    more refactoring

--- client/components/ControlledStoryEditView/index.js
+++ client/components/ControlledStoryEditView/index.js
@@ -6,7 +6,13 @@ import { Button } from 'react-bootstrap'
 import { FormattedMessage, useIntl } from 'react-intl'
 import useWindowDimensions from 'Utilities/windowDimensions'
 import { getStoryAction } from 'Utilities/redux/storiesReducer'
-
+import { clearFocusedSnippet } from 'Utilities/redux/snippetsReducer'
+import {
+  setPrevious,
+  getCurrentSnippetFrozen,
+  getNextSnippetFrozen,
+} from 'Utilities/redux/controlledPracticeReducer'
+import { finishSnippet } from 'Utilities/redux/practiceReducer'
 import { clearTranslationAction } from 'Utilities/redux/translationReducer'
 import { resetAnnotations, setAnnotations } from 'Utilities/redux/annotationsReducer'
 import { learningLanguageSelector, getTextStyle, getMode } from 'Utilities/common'
@@ -25,10 +31,11 @@ const ControlledStoryEditView = ({ match }) => {
   const intl = useIntl()
   const { width } = useWindowDimensions()
   const [hideFeedback, setHideFeedback] = useState(false)
+  const [done, setDone] = useState(false)
   const mode = getMode()
   const history = useHistory()
   const [showRefreshButton, setShowRefreshButton] = useState(false)
-
+  const controlledPractice = useSelector(({ controlledPractice }) => controlledPractice)
   const { story, pending } = useSelector(({ stories, locale }) => ({
     story: stories.focused,
     pending: stories.focusedPending,
@@ -42,6 +49,8 @@ const ControlledStoryEditView = ({ match }) => {
   const learningLanguage = useSelector(learningLanguageSelector)
   const { id } = match.params
 
+  console.log('contr ', controlledPractice)
+
   useEffect(() => {
     if (user?.user.is_teacher) {
       setHideFeedback(false)
@@ -49,7 +58,19 @@ const ControlledStoryEditView = ({ match }) => {
     dispatch(getStoryAction(id, 'preview'))
     dispatch(clearTranslationAction())
     dispatch(resetAnnotations())
+    dispatch(setPrevious([]))
+    dispatch(getCurrentSnippetFrozen(id))
   }, [])
+
+  useEffect(() => {
+    if (
+      controlledPractice?.focused?.total_num === story?.paragraph.length - 1 &&
+      controlledPractice.focused.previous.length === story.paragraph.length - 1
+    ) {
+      console.log('?')
+      dispatch(finishSnippet())
+    }
+  }, [story, controlledPractice])
 
   useEffect(() => {
     if (story) {
@@ -81,6 +102,17 @@ const ControlledStoryEditView = ({ match }) => {
   const refreshPage = () => {
     dispatch(getStoryAction(id, 'preview'))
     setShowRefreshButton(false)
+  }
+
+  const saveControlledStory = () => {
+    setDone(true)
+    dispatch(setPrevious([story.paragraph]))
+    if (story.paragraph.length > 1) {
+      dispatch(
+        getNextSnippetFrozen(id, story.paragraph.length - 1, controlledPractice.acceptedTokens)
+      )
+    }
+    console.log('++')
   }
 
   return (
@@ -148,6 +180,16 @@ const ControlledStoryEditView = ({ match }) => {
                 <hr />
               </>
             ))}
+            <div>
+              <Button
+                variant="primary"
+                onClick={saveControlledStory}
+                type="button"
+                style={{ width: '100%', marginBottom: '.5em' }}
+              >
+                <FormattedMessage id="freeze-and-save-control-story" />
+              </Button>
+            </div>
             <ScrollArrow />
           </Segment>
           {width >= 500 ? (
@@ -170,146 +212,5 @@ const ControlledStoryEditView = ({ match }) => {
     </div>
   )
 }
-
-/*
-import React, { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { FormattedMessage } from 'react-intl'
-import { useParams } from 'react-router-dom'
-import { Segment } from 'semantic-ui-react'
-import { getStoryAction } from 'Utilities/redux/storiesReducer'
-import { clearFocusedSnippet } from 'Utilities/redux/snippetsReducer'
-import { setTouchedIds, setAnswers } from 'Utilities/redux/practiceReducer'
-import { resetAnnotations } from 'Utilities/redux/annotationsReducer'
-import useWindowDimensions from 'Utilities/windowDimensions'
-import { getTextStyle, learningLanguageSelector, getMode } from 'Utilities/common'
-import CurrentSnippet from 'Components/ControlledStoryEditView/CurrentSnippet'
-import DictionaryHelp from 'Components/DictionaryHelp'
-import ReportButton from 'Components/ReportButton'
-import AnnotationBox from 'Components/AnnotationBox'
-import TextWithFeedback from 'Components/CommonStoryTextComponents/TextWithFeedback'
-import PreviousSnippets from './PreviousSnippets'
-import ProgressBar from './CurrentSnippet/ProgressBar'
-import Footer from '../Footer'
-import ScrollArrow from '../ScrollArrow'
-
-const ControlledStoryEditView = () => {
-  const learningLanguage = useSelector(learningLanguageSelector)
-  const dispatch = useDispatch()
-  const { id } = useParams()
-  const { width } = useWindowDimensions()
-  const controlledPractice = useSelector(({ controlledPractice }) => controlledPractice)
-  const smallScreen = width < 700
-  const mode = getMode()
-
-  const controlledPracticeTotalNum = controlledPractice?.focused?.total_num
-
-  const currentSnippetId = () => {
-    if (!controlledPractice.focused) return -1
-    const { snippetid } = controlledPractice.focused
-    return snippetid[snippetid.length - 1] ?? controlledPracticeTotalNum - 1
-  }
-
-  const currentControlledPracticeNum = currentSnippetId() + 1
-
-  const { story, pending } = useSelector(({ stories, locale }) => ({
-    story: stories.focused,
-    pending: stories.focusedPending,
-    locale,
-  }))
-
-  const showAnnotationBox = width >= 1024
-
-  useEffect(() => {
-    dispatch(getStoryAction(id, mode))
-  }, [learningLanguage])
-
-  useEffect(() => {
-    dispatch(resetAnnotations())
-
-    return () => {
-      dispatch(clearFocusedSnippet())
-    }
-  }, [])
-
-  if (!story) return null
-
-  const handleAnswerChange = (value, word) => {
-    const { surface, id, ID, concept } = word
-
-    dispatch(setTouchedIds(ID))
-
-    const newAnswer = {
-      [ID]: {
-        correct: surface,
-        users_answer: value,
-        id,
-        concept,
-      },
-    }
-    dispatch(setAnswers(newAnswer))
-  }
-
-  const showFooter = width > 640
-
-  return (
-    <div className="cont-tall pt-sm flex-col space-between">
-      <div className="justify-center">
-        <div className="cont">
-          <Segment>
-            <div className="progress-bar-cont" style={{ top: smallScreen ? '.25em' : '3.25em' }}>
-              <ProgressBar
-                snippetProgress={currentControlledPracticeNum}
-                snippetsTotal={controlledPracticeTotalNum}
-                progress={(currentControlledPracticeNum / controlledPracticeTotalNum).toFixed(2)}
-              />
-            </div>
-            <div
-              className="story-title"
-              style={{
-                ...getTextStyle(learningLanguage, 'title'),
-                width: '100%',
-              }}
-            >
-              {!pending && `${story.title}`}
-            </div>
-            {story.url && !pending ? (
-              <a target="blank" href={story.url}>
-                <FormattedMessage id="Source" />
-              </a>
-            ) : null}
-            <div>
-              {story.paragraph.map(paragraph => (
-                <>
-                  <TextWithFeedback hideFeedback={false} snippet={paragraph} mode="review" />
-                  <hr />
-                </>
-              ))}
-            </div>
-            <PreviousSnippets />
-            <hr />
-            <CurrentSnippet storyId={id} handleInputChange={handleAnswerChange} />
-            <ScrollArrow />
-          </Segment>
-          {width >= 500 ? (
-            <div className="flex-col align-end" style={{ marginTop: '0.5em' }}>
-              <ReportButton />
-            </div>
-          ) : (
-            <div style={{ marginBottom: '0.5em' }}>
-              <ReportButton />
-            </div>
-          )}
-        </div>
-        <div className="dictionary-and-annotations-cont">
-          <DictionaryHelp />
-          <AnnotationBox />
-        </div>
-      </div>
-      {showFooter && <Footer />}
-    </div>
-  )
-}
-*/
 
 export default ControlledStoryEditView
