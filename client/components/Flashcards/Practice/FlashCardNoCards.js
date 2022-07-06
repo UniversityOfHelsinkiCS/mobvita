342f91d83b9f7de8b83b01e6c712e545c4957506
Valtteri Andström <valtteri.andstrom@gmail.com>
2022-07-06 09:33:16 +0300

    fix broken html message

--- client/components/Flashcards/Practice/FlashCardNoCards.js
+++ client/components/Flashcards/Practice/FlashCardNoCards.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { FormattedMessage } from 'react-intl'
+import { FormattedHTMLMessage } from 'react-intl'
 import SelectLanguage from '../SelectLanguage'
 
 const FlashcardNoCards = ({ setSwipeIndex }) => (
   <div className="flashcard auto">
     <div data-cy="no-flashcards-text" className="flashcard-no-cards">
       <p>
-        <FormattedMessage id="no-flashcards-yet-when-you-practice-a-story-and-click-on-unfamiliar-words-they-will-be-added-to-your" />
+        <FormattedHTMLMessage id="no-flashcards-yet-when-you-practice-a-story-and-click-on-unfamiliar-words-they-will-be-added-to-your" />
       </p>
     </div>
     <div className="flashcard-footer">
