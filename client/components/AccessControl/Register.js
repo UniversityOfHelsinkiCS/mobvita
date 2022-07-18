47407663ed1ab141af7103dcf648bab6ab6afe69
Valtteri Andström <valtteri.andstrom@gmail.com>
2022-07-18 16:27:46 +0300

    new options to registration page

--- client/components/AccessControl/Register.js
+++ client/components/AccessControl/Register.js
@@ -3,17 +3,18 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 import { registerUser } from 'Utilities/redux/registerReducer'
 import { getSelf } from 'Utilities//redux/userReducer'
-import { Form, Checkbox } from 'semantic-ui-react'
+import { Form, Checkbox, Dropdown } from 'semantic-ui-react'
 import TermsAndConditions from 'Components/StaticContent/TermsAndConditions'
-import { useIntl } from 'react-intl'
+import { useIntl, FormattedMessage } from 'react-intl'
 import { setNotification } from 'Utilities/redux/notificationReducer'
-import { localeCodeToName } from 'Utilities/common'
+import { localeCodeToName, supportedLearningLanguages, capitalize } from 'Utilities/common'
 import { Button, Spinner } from 'react-bootstrap'
 
 const Register = () => {
   const intl = useIntl()
   const history = useHistory()
-
+  const [userRole, setUserRole] = 'student'
+  const [chosenLanguage, setChosenLanguage] = useState('Finnish')
   const [formState, setFormState] = useState({
     email: '',
     username: '',
@@ -25,6 +26,14 @@ const Register = () => {
   const toggleAccepted = () => {
     setAccepted(!accepted)
   }
+  const mergeList = supportedLearningLanguages.major
+    .concat(supportedLearningLanguages.majorBeta)
+    .concat(supportedLearningLanguages.minor)
+    .map(language => ({
+      key: language,
+      text: capitalize(language),
+      value: JSON.stringify(language), // needs to be string
+    }))
 
   const {
     error,
@@ -120,7 +129,49 @@ const Register = () => {
             placeholder={intl.formatMessage({ id: 'repeat-password' })}
           />
         </Form.Field>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div>
+          <button
+            type="button"
+            onClick={() => setUserRole('student')}
+            style={{ border: 'none', backgroundColor: '#FFFFFF' }}
+          >
+            <div className="flex align-center" style={{ gap: '.5em' }}>
+              <input
+                type="radio"
+                onChange={() => setUserRole('student')}
+                checked={userRole === 'studnet'}
+              />
+              <FormattedMessage id="user-role-select-student" />
+            </div>
+          </button>
+          <button
+            type="button"
+            onClick={() => setUserRole('teacher')}
+            style={{ border: 'none', backgroundColor: '#FFFFFF' }}
+          >
+            <div className="flex align-center" style={{ gap: '.5em' }}>
+              <input
+                type="radio"
+                onChange={() => setUserRole('teacher')}
+                checked={userRole === 'teacher'}
+              />
+              <FormattedMessage id="user-role-select-teacher" />
+            </div>
+          </button>
+        </div>
+        <hr />
+        <div className="flex">
+          <FormattedMessage id="Learning-language" />
+          <Dropdown
+            selection
+            fluid
+            options={mergeList}
+            text={chosenLanguage}
+            onChange={(_, { value }) => setChosenLanguage(capitalize(JSON.parse(value)))}
+          />
+        </div>
+        <hr />
+        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '0.5em' }}>
           <Checkbox data-cy="accept-terms" checked={accepted} onChange={() => toggleAccepted()} />
           <TermsAndConditions
             trigger={<Button variant="link"> Terms and Conditions, Privacy Policy </Button>}
