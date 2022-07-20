a16218c96a85ec7e7e4f6c7689afc16fb69e0976
Valtteri Andström <valtteri.andstrom@gmail.com>
2022-07-20 12:33:05 +0300

    change the color of flash card edit view

--- client/components/Flashcards/Template/TemplateDesktopView.js
+++ client/components/Flashcards/Template/TemplateDesktopView.js
@@ -6,11 +6,14 @@ import TemplateActions from './TemplateActions'
 
 const TemplateDesktopView = props => (
   <div className="flex">
-    <div className="flashcard">
+    <div className="flashcard" style={{ backgroundColor: '#FFEFD5', color: '#000000' }}>
       <TemplateWord {...props} />
       <TemplateHints {...props} />
     </div>
-    <div className="flashcard ml-lg" style={{ marginLeft: '1em' }}>
+    <div
+      className="flashcard ml-lg"
+      style={{ marginLeft: '1em', backgroundColor: '#FFEFD5', color: '#000000' }}
+    >
       <TemplateTranslations bigScreen {...props} />
       <TemplateActions {...props} />
     </div>
