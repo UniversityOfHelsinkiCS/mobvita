2291b7734ff83764cc9ae71963f0c913844730f0
Valtteri Andström <valtteri.andstrom@gmail.com>
2022-08-22 14:13:21 +0300

    add collapse/expand toggle to topics box

--- client/components/StoryView/StoryTopics.js
+++ client/components/StoryView/StoryTopics.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import useWindowDimensions from 'Utilities/windowDimensions'
+import { Icon } from 'semantic-ui-react'
 import { FormattedMessage } from 'react-intl'
 
 const StoryTopics = ({ conceptCount }) => {
   const [topTopics, setTopTopics] = useState([])
   const { width } = useWindowDimensions()
+  const [collapsed, setCollapsed] = useState(false)
 
   useEffect(() => {
     const keysSorted = Object.entries(conceptCount).sort((a, b) => {
@@ -17,16 +19,28 @@ const StoryTopics = ({ conceptCount }) => {
     return (
       <div className="story-topics-box" style={{ padding: '1em' }}>
         <div style={{ backgroundColor: '#FFFFFF' }}>
-          <div className="header-3" style={{ marginBottom: '.5em' }}>
-            <FormattedMessage id="story-top-topics" />
+          <div className="flex space-between">
+            <div style={{ marginBottom: '.5em' }}>
+              <FormattedMessage id="story-top-topics" />:
+            </div>
+            <div
+              onClick={() => setCollapsed(!collapsed)}
+              onKeyDown={() => setCollapsed(!collapsed)}
+              role="button"
+              tabIndex={0}
+            >
+              <Icon name={collapsed ? 'angle up' : 'angle down'} size="large" />
+            </div>
           </div>
-          <ul style={{ overflow: 'auto', maxHeight: 120 }}>
-            {topTopics.map(topic => (
-              <li>
-                {topic[0]}: {topic[1]}
-              </li>
-            ))}
-          </ul>
+          {!collapsed && (
+            <ul style={{ overflow: 'auto', maxHeight: 120 }}>
+              {topTopics.map(topic => (
+                <li>
+                  {topic[0]}: {topic[1]}
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       </div>
     )
