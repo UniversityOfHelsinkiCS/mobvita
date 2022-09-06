dcbe0d3584322dc7c0c92dc1ff39cef68100c31d
Valtteri Andström <valtteri.andstrom@gmail.com>
2022-09-06 10:31:01 +0300

    first version of the student cefr modal

--- client/components/GroupView/StudentCEFRModal.js
+++ client/components/GroupView/StudentCEFRModal.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Icon } from 'semantic-ui-react'
+import { Table } from 'react-bootstrap'
 import Draggable from 'react-draggable'
+import { skillLevels, capitalize } from 'Utilities/common'
+import moment from 'moment'
 
 const StudentCEFRModal = ({ open, setOpen, cefrHistory }) => {
-
   const closeModal = () => {
     setOpen(false)
   }
-  console.log('his ', cefrHistory)
+  const date = moment.unix(cefrHistory[0].timestamp).format("MM/DD/YYYY")
+  console.log('date ', date)
+  
   if (open) {
     return (
       <Draggable cancel=".interactable">
@@ -24,7 +28,24 @@ const StudentCEFRModal = ({ open, setOpen, cefrHistory }) => {
               onClick={closeModal}
             />
           </div>
-          MODAALI
+          <Table striped bordered hover responsive size="sm">
+            <thead>
+              <tr key="summary-header-row">
+                <th style={{ textAlign: 'center', verticalAlign: 'middle' }}>Date</th>
+                <th>Estimator</th>
+                <th>Grade</th>
+              </tr>
+              {cefrHistory.map(estimate => (
+                <tr>
+                  <th>{moment.unix(estimate.timestamp).format("MM/DD/YYYY")}</th>
+                  <th>
+                    {estimate.source === 'self_estimation' ? 'Self' : capitalize(estimate.source)}
+                  </th>
+                  <th>{skillLevels[estimate.grade]}</th>
+                </tr>
+              ))}
+            </thead>
+          </Table>
         </div>
       </Draggable>
     )
