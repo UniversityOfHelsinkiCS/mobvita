caf9b7a76bbf5e3295ec8e2aba977fa7fee47182
Valtteri Andström <valtteri.andstrom@gmail.com>
2022-09-12 13:03:46 +0300

    bring percentage graph back to staging

--- client/components/VocabularyView/VocabularyGraph.js
+++ client/components/VocabularyView/VocabularyGraph.js
@@ -71,7 +71,153 @@ const VocabularyGraph = ({
   const numMastered = currentPerc?.vocab_bins?.reduce((prev, curr) => prev + curr.mastered, 0)
   const numNotMastered = notMastered?.reduce((prev, curr) => prev + curr, 0)
 
-  const [series, setSeries] = useState([
+  const [series, setSeries] = useState(hiddenFeatures ? [
+    {
+      name: `${intl.formatMessage({ id: 'not-mastered' })} ${intl.formatMessage({
+        id: 'vocabulary-follow-statistic-before',
+      })}`,
+      id: 'Not Mastered (before)',
+      data: getNotMasteredData(previousPerc?.vocab_bins),
+      linkedTo: 'Mastered',
+      visible: false,
+      stack: 'before',
+      color: '#FAA0A0',
+    },
+    {
+      name: `${intl.formatMessage({ id: 'rewardable-words' })} ${intl.formatMessage({
+        id: 'vocabulary-follow-statistic-before',
+      })}`,
+      id: 'Mastered (before rewardable)',
+      data: previousPerc?.vocab_bins?.map(v => v.rewardable),
+      linkedTo: 'Mastered',
+      visible: false,
+      color: '#5FBDC2',
+      stack: 'before',
+    },
+    {
+      name: `${intl.formatMessage({ id: 'mastered-words' })} ${intl.formatMessage({
+        id: 'vocabulary-follow-statistic-before',
+      })}`,
+      id: 'Mastered (before)',
+      data: previousPerc?.vocab_bins?.map(v => v.mastered),
+      linkedTo: 'Mastered',
+      visible: false,
+      color: '#90EE90',
+      stack: 'before',
+    },
+    {
+      name: `${intl.formatMessage({ id: 'not-mastered' })}`,
+      id: 'Overview (not mastered)',
+      data: notMastered,
+      linkedTo: 'Mastered',
+      color: '#DC143C',
+      stack: 'present',
+    },
+    {
+      name: `${intl.formatMessage({ id: 'rewardable-words' })}`,
+      id: 'Overview (rewardable)',
+      data: currentPerc?.vocab_bins?.map(v => v.rewardable),
+      linkedTo: 'Mastered',
+      color: '#4169e1',
+      stack: 'present',
+    },
+    {
+      name: `${intl.formatMessage({ id: 'mastered-words' })}`,
+      id: 'Overview',
+      data: currentPerc?.vocab_bins?.map(v => v.mastered),
+      color: '#228B22',
+      stack: 'present',
+    },
+    {
+      name: intl.formatMessage({ id: 'vocabulary-total' }),
+      id: 'Total',
+      data: newTotal,
+      visible: false,
+    },
+    {
+      name: `${intl.formatMessage({ id: 'vocabulary-total' })} ${intl.formatMessage({
+        id: 'vocabulary-follow-statistic-before',
+      })}`,
+      id: 'Total (before)',
+      data: total,
+      linkedTo: 'Total',
+      visible: false,
+    },
+    /* 
+    {
+      name: intl.formatMessage({ id: 'vocabulary-seen' }),
+      id: 'Seen',
+      data: newSeen,
+      visible: false,
+    },
+    {
+      name: `${intl.formatMessage({ id: 'vocabulary-seen' })} ${intl.formatMessage({
+        id: 'vocabulary-follow-statistic-before',
+      })}`,
+      id: 'Seen (before)',
+      data: seen,
+      linkedTo: 'Seen',
+      visible: false,
+    },
+    {
+      name: intl.formatMessage({ id: 'vocabulary-visit' }),
+      id: 'Visit',
+      data: newVisit,
+      visible: false,
+    },
+    {
+      name: `${intl.formatMessage({ id: 'vocabulary-visit' })} ${intl.formatMessage({
+        id: 'vocabulary-follow-statistic-before',
+      })}`,
+      id: 'Visit (before)',
+      data: visit,
+      linkedTo: 'Visit',
+      visible: false,
+    },
+    */
+    {
+      name: intl.formatMessage({ id: 'vocabulary-flashcard' }),
+      id: 'Flashcard',
+      data: newFlashcard,
+      visible: false,
+    },
+    {
+      name: `${intl.formatMessage({ id: 'vocabulary-flashcard' })} ${intl.formatMessage({
+        id: 'vocabulary-follow-statistic-before',
+      })}`,
+      id: 'Flashcard (before)',
+      data: flashcard,
+      linkedTo: 'Flashcard',
+      visible: false,
+    },
+    {
+      name: `${intl.formatMessage({ id: 'percent-graph' })} ${intl.formatMessage({
+        id: 'vocabulary-follow-statistic-before',
+      })}`,
+      data: previousPerc?.vocab_bins?.map(v => v.mastering_percentage),
+      id: 'Percentage (before)',
+      linkedTo: 'Percentage',
+      color: '#90EE90',
+      visible: false,
+      stack: 'before',
+    },
+    {
+      name: `${intl.formatMessage({ id: 'percent-graph' })}`,
+      id: 'Curr Percentage',
+      data: currentPerc.vocab_bins.map(v => v.mastering_percentage),
+      color: '#228B22',
+      visible: false,
+      stack: 'present',
+    },
+    {
+      name: `${intl.formatMessage({ id: 'target-curve' })}`,
+      id: 'Curr Percentage target',
+      data: getTargetCurve(),
+      linkedTo: 'Percentage',
+      type: 'spline',
+      visible: false,
+    },
+  ] : [
     {
       name: `${intl.formatMessage({ id: 'not-mastered' })} ${intl.formatMessage({
         id: 'vocabulary-follow-statistic-before',
@@ -252,12 +398,12 @@ const VocabularyGraph = ({
 
   const handlePercentageToggle = () => {
     const copySeries = [...series]
-    if (element?.current.chart.series[10].userOptions.visible) {
+    if (toggleOn) {
       handleHide(copySeries[10], 10)
       setSeries(copySeries)
       setToggleOn(false)
     } else {
-      handleShow(copySeries[14], 14)
+      handleShow(copySeries[10], 10)
 
       setSeries(copySeries)
       setToggleOn(true)
@@ -355,7 +501,6 @@ const VocabularyGraph = ({
         marker: { enabled: true },
         events: {
           legendItemClick() {
-            // console.log('toggle at click ', toggleOn)
             setToggleOn(false)
             if (this.userOptions.id === 'Overview') {
               setGraphType('column mastered')
