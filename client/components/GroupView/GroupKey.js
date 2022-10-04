6875288df2b76038b5c47ff7a5b4a0a27db5325a
Valtteri Andström <valtteri.andstrom@gmail.com>
2022-10-04 11:42:22 +0300

    separate group key into its own component

---
+++ client/components/GroupView/GroupKey.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { setNotification } from 'Utilities/redux/notificationReducer'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+import { FormattedMessage } from 'react-intl'
+import { Icon } from 'semantic-ui-react'
+import { Button } from 'react-bootstrap'
+
+const GroupKey = () => {
+  const dispatch = useDispatch()
+  const token = useSelector(state => state.groups.token)
+
+  const handleTokenCopy = () => {
+    dispatch(setNotification('token-copied', 'info'))
+  }
+
+  return (
+    <div>
+      <div
+        className="border rounded"
+        style={{
+          display: 'flex',
+          marginTop: '0.5em',
+          minHeight: '3em',
+          wordBreak: 'break-all',
+        }}
+      >
+        <span style={{ margin: 'auto', padding: '0.5em' }}>{token}</span>
+        <CopyToClipboard text={token}>
+          <Button type="button" onClick={handleTokenCopy}>
+            <Icon name="copy" size="large" />
+          </Button>
+        </CopyToClipboard>
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <span
+          style={{
+            margin: 'auto',
+            padding: '0.5em',
+            fontStyle: 'oblique',
+            fontWeight: 'bold',
+          }}
+        >
+          <FormattedMessage id="This key is valid for the next 30 days" />.
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default GroupKey
