2c7a8ee59fa67ad611740d92d9157ce9f17db33c
Valtteri Andström <valtteri.andstrom@gmail.com>
2022-11-04 15:18:27 +0200

    change lesson practice arrows

--- client/components/Lessons/LessonPracticeList.js
+++ client/components/Lessons/LessonPracticeList.js
@@ -1,9 +1,55 @@
+/* eslint-disable no-nested-ternary */
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
 import { Icon } from 'semantic-ui-react'
 
-const LessonPracticeList = ({ lessonsPractices, removePractice }) => {
+const ChangePracticeOrderArrows = ({ index, pracLength }) => {
+  // console.log(index, ' and  ', pracLength - 1)
+  return index === 0 ? (
+    <Icon
+      style={{
+        cursor: 'pointer',
+        marginBottom: '.25em',
+        color: 'black',
+      }}
+      size="large"
+      name="angle down"
+    />
+  ) : index < pracLength - 1 ? (
+    <>
+      <Icon
+        style={{
+          cursor: 'pointer',
+          marginBottom: '.25em',
+          color: 'black',
+        }}
+        size="large"
+        name="angle up"
+      />
+      <Icon
+        style={{
+          cursor: 'pointer',
+          marginBottom: '.25em',
+          color: 'black',
+        }}
+        size="large"
+        name="angle down"
+      />
+    </>
+  ) : (
+    <Icon
+      style={{
+        cursor: 'pointer',
+        marginBottom: '.25em',
+        color: 'black',
+      }}
+      size="large"
+      name="angle up"
+    />
+  )
+}
 
+const LessonPracticeList = ({ lessonsPractices, removePractice }) => {
   return (
     <div style={{ marginBottom: '.5rem' }}>
       {lessonsPractices.length < 1 && <FormattedMessage id="no-practices-yet" />}
@@ -13,17 +59,21 @@ const LessonPracticeList = ({ lessonsPractices, removePractice }) => {
             <b style={{ marginRight: '1rem' }}>{index + 1}.</b>
             <FormattedMessage id={practice} />
           </div>
-          <Icon
-            className="interactable"
-            style={{
-              cursor: 'pointer',
-              marginBottom: '.25em',
-              color: 'red',
-            }}
-            size="large"
-            name="close"
-            onClick={() => removePractice(index)}
-          />
+          <div>
+            {lessonsPractices.length > 1 && (
+              <ChangePracticeOrderArrows index={index} pracLength={lessonsPractices.length} />
+            )}
+            <Icon
+              style={{
+                cursor: 'pointer',
+                marginBottom: '.25em',
+                color: 'red',
+              }}
+              size="large"
+              name="close"
+              onClick={() => removePractice(index)}
+            />
+          </div>
         </div>
       ))}
     </div>
