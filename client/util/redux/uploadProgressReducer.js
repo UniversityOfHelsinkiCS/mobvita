4a463e7fa2cf661341a82d7fdeb8913778e58ea9
jue.hou <hj940709@gmail.com>
2023-03-02 13:35:28 +0200

    debug uploading story progress

--- client/util/redux/uploadProgressReducer.js
+++ client/util/redux/uploadProgressReducer.js
@@ -66,11 +66,10 @@ export default (state = initialState, action) => {
         error: true,
       }
     case 'POST_NEW_STORY_SUCCESS':
-      let story_id = action.response.story_ids?.length > 0 ? action.response.story_ids[action.response.story_ids.length - 1] : null
       return {
         ...state,
         pending: false,
-        storyId: story_id,
+        storyId: action.response.story_ids?.length > 0 ? action.response.story_ids[action.response.story_ids.length - 1] : null,
         progress: 0,
         error: false,
         custom: false,
@@ -93,7 +92,7 @@ export default (state = initialState, action) => {
       return {
         ...state,
         pending: false,
-        storyId: action.response.story_id,
+        storyId: action.response.story_ids?.length > 0 ? action.response.story_ids[action.response.story_ids.length - 1] : null,
         progress: 0,
         error: false,
         custom: false,
