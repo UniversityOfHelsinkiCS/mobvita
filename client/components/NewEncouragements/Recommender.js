01fd9b859037097c879728df297f1f33ae3e6961
qusba <kasper.kahkonen@outlook.com>
2023-03-19 10:30:28 +0200

    created flashcardencouragement for homeview for the new recommendations system

--- client/components/NewEncouragements/Recommender.js
+++ client/components/NewEncouragements/Recommender.js
@@ -6,6 +6,7 @@ import { Icon } from 'semantic-ui-react'
 import { closeEncouragement } from 'Utilities/redux/encouragementsReducer'
 import { getLeaderboards } from 'Utilities/redux/leaderboardReducer'
 import { getIncompleteStories } from 'Utilities/redux/incompleteStoriesReducer'
+import HomeViewFlashcardEncouragement from 'Components/NewEncouragements/SubComponents/HomeViewFlashcardEncouragement'
 import UnseenStoriesInGroup from './SubComponents/UnseenStoriesInGroup'
 import LatestIncompleteStory from './SubComponents/LatestIncompleteStory'
 import TurnOffRecommendations from './SubComponents/TurnOffRecommendations'
@@ -73,6 +74,7 @@ const Recommender = () => {
             <LeaderboardEncouragement />
             <DailyStoriesEncouragement handleDailyStoriesClick={handleDailyStoriesClick} />
             <LatestIncompleteStory enable_recmd={enable_recmd} />
+            <HomeViewFlashcardEncouragement />
             <UnseenStoriesInGroup />
             <TurnOffRecommendations />
           </div>
