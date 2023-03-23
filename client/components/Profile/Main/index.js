301080d956a54d43953895ed08ef74aad52679fa
Matti <matti.kahkonen99@gmail.com>
2023-03-23 15:10:22 +0200

    add a layout to profile page
    Co-authored-by: Noora <98142487+NooraKemp@users.noreply.github.com>

--- client/components/Profile/Main/index.js
+++ client/components/Profile/Main/index.js
@@ -2,13 +2,49 @@ import React from 'react'
 import Following from '../Following'
 import EloChart from 'Components/HomeView/EloChart'
 import MedalSummary from 'Components/HomeView/MedalSummary'
+import useWindowDimensions from 'Utilities/windowDimensions'
+import { Grid } from 'semantic-ui-react'
 
-const Main = () => {
+const DesktopView = () => {
   return (
-    <>
+    <div>
+      <Grid columns={2} divided>
+        <Grid.Row>
+          <Grid.Column>
+            <EloChart />
+          </Grid.Column>
+          <Grid.Column>
+            <Following />
+            </Grid.Column>
+        </Grid.Row>
+      </Grid>
+      <div>
+        <MedalSummary />
+      </div>
+    </div>
+  )
+}
+
+const MobileView = () => {
+  return (
+    <div>
       <Following />
       <EloChart width="100%" />
       <MedalSummary />
+    </div>
+  )
+}
+
+const Main = () => {
+  const { width } = useWindowDimensions()
+  const bigScreen = width >= 700
+  return (
+    <>
+    {bigScreen ? (
+      <DesktopView />
+    ) : (
+      <MobileView />
+    )}
     </>
   )
 }
