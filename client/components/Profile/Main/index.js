59e6c9c1c9488d068ad8d95d49a63912b99325a0
Matti <matti.kahkonen99@gmail.com>
2023-03-25 14:54:24 +0200

    modify profile page
    Co-authored-by: Noora <98142487+NooraKemp@users.noreply.github.com>

--- client/components/Profile/Main/index.js
+++ client/components/Profile/Main/index.js
@@ -3,24 +3,54 @@ import Following from '../Following'
 import EloChart from 'Components/HomeView/EloChart'
 import MedalSummary from 'Components/HomeView/MedalSummary'
 import useWindowDimensions from 'Utilities/windowDimensions'
+import { useSelector } from 'react-redux'
 import { Grid } from 'semantic-ui-react'
+import { FormattedMessage } from 'react-intl'
+import ProgressStats from '../Progress/ProgressStats'
+import moment from 'moment'
+
+/* const ProgressStatistics = () => {
+  const { data } = useSelector(({ user }) => user.data)
+  const { exerciseHistoryGraph } = useSelector(({ user }) => user.data.user.exercise_history)
+  console.log(data);
+  console.log(exerciseHistoryGraph);
+  const startDate = moment(exerciseHistoryGraph[0]?.date).toDate()
+  const endDate = Date().toLocaleString()
+
+  console.log(endDate);
+
+  return (
+    <ProgressStats startDate={startDate} endDate={endDate} />
+  )
+} */
+
+const UsernameField = () => {
+  const { username } = useSelector(({ user }) => user.data.user)
+  return (
+    <>
+      <span className="sm-label">
+        <FormattedMessage id="username" />
+      </span>
+      <p className="account-info-item">{username}</p>
+    </>
+  )
+}
 
 const DesktopView = () => {
   return (
     <div>
-      <Grid columns={2} divided>
+      <Grid columns={2}  divided>
         <Grid.Row>
           <Grid.Column>
+            <UsernameField />
             <EloChart />
+            <MedalSummary />
           </Grid.Column>
           <Grid.Column>
             <Following />
             </Grid.Column>
         </Grid.Row>
       </Grid>
-      <div>
-        <MedalSummary />
-      </div>
     </div>
   )
 }
@@ -28,6 +58,7 @@ const DesktopView = () => {
 const MobileView = () => {
   return (
     <div>
+      <UsernameField />
       <Following />
       <EloChart width="100%" />
       <MedalSummary />
