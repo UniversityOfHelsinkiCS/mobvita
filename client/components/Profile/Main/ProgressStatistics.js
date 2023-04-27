7f9d825af66223f37d072f11ed949b7be063be03
VoxBorealis <uusitalo.y@gmail.com>
2023-04-27 22:47:45 +0300

    fix profile/progress page reducer to show data correctly

--- client/components/Profile/Main/ProgressStatistics.js
+++ client/components/Profile/Main/ProgressStatistics.js
@@ -1,17 +1,8 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 import ProgressStats from '../Progress/ProgressStats'
-import moment from 'moment'
 
 const ProgressStatistics = () => {
-  const practiceHistory = useSelector(state => state.practiceHistory)
-  const { exerciseHistory } = practiceHistory
-
-  const endDate = moment().toDate()
-
-  const startDate = moment(exerciseHistory[0]?.date).toDate()
-
-  return <ProgressStats startDate={startDate} endDate={endDate} />
+  return <ProgressStats />
 }
 
 export default ProgressStatistics
