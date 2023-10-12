dd1bfe6ba0e9878c777218e500db1fecf8e06098
duc.vu <anh-duc.vu@younite-ai.com>
2023-10-12 15:25:02 +0300

    update spec

--- cypress/e2e/mobvita_spec.js
+++ cypress/e2e/mobvita_spec.js
@@ -140,7 +140,7 @@ describe('Mobvita', function () {
     */
     describe("stories", function () {
       this.beforeEach(function () {
-        cy.get('[data-cy=practice-now]').click()
+        // cy.get('[data-cy=practice-now]').click()
         cy.get("[data-cy=goto-library]")
           .click()
       })
