c9296ebac48a33a43b8159f1595d46983feb357e
advu <anh-duc.vu@f-secure.com>
2024-10-03 12:22:25 +0300

    minro fix

--- client/components/Tests/ReadingTest/ReadingTest.js
+++ client/components/Tests/ReadingTest/ReadingTest.js
@@ -326,8 +326,8 @@ const ReadingTest = () => {
   useEffect(() => {
     setShowFeedbacks(false)
     if (currentReadingSet !== null && prevReadingSet !== null && currentReadingSet !== prevReadingSet) {
-      const currentSet = readingTestSetDict[currentReadingSet]
-      if (currentSet && currentSet.collect_final_reflection) {
+      const prevSet = readingTestSetDict[prevReadingSet]
+      if (prevSet && prevSet.collect_final_reflection) {
         if (in_experimental_grp && receivedFeedback > 0) {
           setShowSelfReflect(!resumedTest)
         }
