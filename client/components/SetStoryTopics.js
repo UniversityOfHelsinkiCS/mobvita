ecc839960020a7a3fc2d9ef23aa74e73fc1ef00b
jue.hou <hj940709@gmail.com>
2025-02-19 16:32:26 +0200

    candidate count in story topics setting

--- client/components/SetStoryTopics.js
+++ client/components/SetStoryTopics.js
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect } from 'react'
 import { Link, useHistory, useParams } from 'react-router-dom'
+import { FormattedMessage } from 'react-intl'
 import { Icon} from 'semantic-ui-react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getStoryAction, updateExerciseTopics, updateTempExerciseTopics } from 'Utilities/redux/storiesReducer'
@@ -35,12 +36,17 @@ const SetStoryTopics = ({ match }) => {
 
     return (
         <div className="cont-tall pt-lg cont flex-col auto gap-row-sm ">
-            <Link to={`/stories/${id}/preview`} className="link-primary">
-                <h3>
-                    <Icon name="arrow left" />
-                    {story.title}
-                </h3>
-            </Link>
+            <div class="row">
+                <Link to={`/stories/${id}/preview`} className="col-md-8 link-primary">
+                    <h3 style={{textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap'}}>
+                        <Icon name="arrow left" />
+                        {story.title}
+                    </h3>
+                </Link>
+                <h4 className="col-md-4 text-right">
+                    <FormattedMessage id="candidate-available" values={{candidate_count: story.candidate_count}}/>
+                </h4>
+            </div>
             <br />
             <br />
             <Topics
