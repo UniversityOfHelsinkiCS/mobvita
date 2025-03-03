2d25f9d6ca2212b871c80b6258a2c499ba04325b
jue.hou <jue.hou@helsinki.fi>
2025-03-03 15:51:53 +0200

    debug CI test

--- cypress/e2e/dictionary_spec.js
+++ cypress/e2e/dictionary_spec.js
@@ -9,8 +9,8 @@ describe("dictionary", function () {
     cy.viewport(1920, 1080)
     cy.loginExisting(this.user)
     cy.visit("http://localhost:8000/stories/5c407e9eff634503466b0dde/preview")
-    cy.get('[data-cy=dictionary-icon]') // Open dictionaryhelp
-      .click({ force: true })
+    // cy.get('[data-cy=dictionary-icon]') // Open dictionaryhelp
+    //   .click({ force: true })
   })
 
   this.afterAll(function () {
