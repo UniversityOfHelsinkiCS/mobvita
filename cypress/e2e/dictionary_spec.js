3c6b754cae1dc0b5fee5a33a93918836bd9040dd
jue.hou <jue.hou@helsinki.fi>
2025-03-03 15:45:41 +0200

    debug CI test

--- cypress/e2e/dictionary_spec.js
+++ cypress/e2e/dictionary_spec.js
@@ -6,7 +6,7 @@ describe("dictionary", function () {
   })
 
   this.beforeEach(function () {
-    console.log()
+    cy.viewport(1920, 1080)
     cy.loginExisting(this.user)
     cy.visit("http://localhost:8000/stories/5c407e9eff634503466b0dde/preview")
     cy.get('[data-cy=dictionary-icon]') // Open dictionaryhelp
