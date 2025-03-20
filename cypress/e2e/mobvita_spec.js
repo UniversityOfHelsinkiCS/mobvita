a328305899f2f738fae9f47831b489143943f581
Robert Pakkanen <rpakkane@gmail.com>
2025-03-20 12:19:50 +0200

    Add selection of language for translation test

--- cypress/e2e/mobvita_spec.js
+++ cypress/e2e/mobvita_spec.js
@@ -176,6 +176,7 @@ describe('Mobvita', function () {
 
       it("can click (translate) a word", function () { // This test overlaps with dictionary
         cy.contains("ehkä").click()
+        cy.get("[data-cy=dictionary-dropdown]").select("Suomi")
         cy.contains("ilmaisee jonkin mahdollista tapahtumista tai toteutumista", { timeout: 20000 })
       })
       // it("can add and see annotations in the library", function () {
