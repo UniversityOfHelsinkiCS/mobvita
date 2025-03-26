99499a4f711e818d480fc9cf3c44e4f870cb0c33
Robert Pakkanen <rpakkane@gmail.com>
2025-03-26 13:29:35 +0200

    Add text complexity estimation functionality and table for result

--- client/components/Estimator.js
+++ client/components/Estimator.js
@@ -1,27 +1,77 @@
 import React, { useState } from 'react'
-import { Button, Form } from 'react-bootstrap'
+import { Container, Row, Col, Button, Form, Table } from 'react-bootstrap'
 
 const Estimator = () => {
-  const [story, setStory] = useState('')
+  const [text, setText] = useState('')
+  const [difficulty, setDifficulty] = useState(null)
+
+  const estimate = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ text }),
+      })
+      const data = await response.json()
+      setDifficulty(data.id)
+      console.log('API Response:', data)
+    } catch (error) {
+      console.error('Error submitting text:', error)
+    }
+  }
+
+  const handleClick = e => {
+    e.preventDefault()
+    estimate()
+  }
 
   return (
-    <div style={{ textAlign: 'center', margin: '20px' }}>
-      <Form>
-        <Form.Group controlId="exampleForm.ControlTextarea1">
-          <Form.Label size="lg">Estimator</Form.Label>
-          <Form.Control
-            as="textarea"
-            rows={5}
-            size="lg"
-            placeholder="Insert your story here"
-            onChange={e => setStory(e.target.value)}
-          />
-        </Form.Group>
-        <Button type="button" onClick={() => console.log(story)}>
-          Submit
-        </Button>
-      </Form>
-    </div>
+    <Container>
+      <Row className="justify-content-center my-5">
+        <Col xs="auto">
+          <h3>Estimate the complexity of Finnish text</h3>
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Col>
+          <Form>
+            <Form.Group>
+              <Form.Control
+                as="textarea"
+                className="overflow-auto"
+                rows={5}
+                size="lg"
+                placeholder="Paste your text here"
+                onChange={e => setText(e.target.value)}
+              />
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Col xs="auto">
+          <Button type="button" onClick={e => handleClick(e)}>
+            Estimate
+          </Button>
+        </Col>
+      </Row>
+      {difficulty && (
+        <Row className="my-5">
+          <Col>
+            <Table bordered>
+              <tbody>
+                <tr>
+                  <td>Difficulty</td>
+                  <td>{difficulty}</td>
+                </tr>
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      )}
+    </Container>
   )
 }
 
