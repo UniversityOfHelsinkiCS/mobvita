c11ad6f23c528d358bbd8190709d5a494156c4b9
Robert Pakkanen <rpakkane@gmail.com>
2025-05-28 11:30:11 +0300

    Clean up onboarding modals

--- client/components/SetCEFRReminder.js
+++ client/components/SetCEFRReminder.js
@@ -2,14 +2,18 @@ import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { useDispatch, useSelector } from 'react-redux';
 import { Modal } from 'semantic-ui-react';
-import { Button, ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
+
+import { useLearningLanguage } from 'Utilities/common'
+import { InitAdaptiveTest } from 'Utilities/redux/testReducer'
 import { updateUserGrade, updateIsTeacher, updateToNonNewUser } from 'Utilities/redux/userReducer';
 import CERFLevelSlider from './CEFRLevelSlider';
 
 const SetCEFRReminder = ({ open, setOpen, newUser }) => {
   const history = useHistory();
   const dispatch = useDispatch();
+  const learningLanguage = useLearningLanguage()
   const [sliderValue, setSliderValue] = useState(121);
   const [isTeacher, setIsTeacher] = useState(false);
   const { hasAdaptiveTests, pending } = useSelector(({ metadata }) => metadata);
@@ -21,23 +25,30 @@ const SetCEFRReminder = ({ open, setOpen, newUser }) => {
   };
 
   const submitSettings = () => {
-    if (isTeacher) {
-      dispatch(updateIsTeacher(true));
-      closeModal();
-    } else {
-      const minified = sliderValue / 11;
-      const rounded = Math.floor(minified / 10);
-      dispatch(updateUserGrade(rounded));
-      dispatch(updateIsTeacher(false));
-      closeModal();
-    }
+    const minified = sliderValue / 11;
+    const rounded = Math.floor(minified / 10);
+    dispatch(updateUserGrade(rounded));
+    dispatch(updateIsTeacher(false));
+    closeModal();
   };
 
   const startAdaptiveTest = () => {
     dispatch(updateIsTeacher(false));
+    dispatch(InitAdaptiveTest(learningLanguage))
     closeModal();
     history.push('/adaptive-test');
   };
+
+  const handleStudentClick = () => {
+    setIsTeacher(false)
+    setStep(step + 1)
+  }
+
+  const handleTeacherClick = () => {
+    setIsTeacher(true)
+    dispatch(updateIsTeacher(true))
+    closeModal()
+  }
 
   if (pending) return null;
 
@@ -47,17 +58,35 @@ const SetCEFRReminder = ({ open, setOpen, newUser }) => {
         <div className="encouragement" style={{ padding: '1.5rem', textAlign: 'center' }}>
           {step === 1 && (
             <>
-              <h3><FormattedMessage id="user-role-select" /></h3>
-              <ToggleButtonGroup type="radio" name="role" value={isTeacher} onChange={setIsTeacher} className="d-flex justify-content-center" style={{ transition: '0.3s' }}>
-                <ToggleButton variant={!isTeacher ? 'primary' : 'secondary'} value={false}><FormattedMessage id="user-role-select-student" /></ToggleButton>
-                <ToggleButton variant={isTeacher ? 'primary' : 'secondary'} value={true}><FormattedMessage id="user-role-select-teacher" /></ToggleButton>
-              </ToggleButtonGroup>
+              <h3 style={{ marginBottom: '40px' }}>
+                <FormattedMessage id="user-role-select" />
+              </h3>
+              <Button
+                variant="primary"
+                size="lg"
+                onClick={handleStudentClick}
+                style={{ marginRight: '20px' }}
+              >
+                <FormattedMessage id="user-role-select-student" />
+              </Button>
+              <Button variant="primary" size="lg" onClick={handleTeacherClick}>
+                <FormattedMessage id="user-role-select-teacher" />
+              </Button>
             </>
           )}
           {step === 2 && !isTeacher && (
             <>
-              <h3><FormattedMessage id="select-cefr-reminder" /></h3>
-              <div style={{ display: 'flex', justifyContent: 'center', gap: '20px' }}>
+              <h3>
+                <FormattedMessage id="select-cefr-reminder" />
+              </h3>
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  gap: '20px',
+                  marginTop: '30px',
+                }}
+              >
                 <Button variant="primary" size="lg" onClick={() => setStep(3)}>
                   <FormattedMessage id="set-cefr-manually" />
                 </Button>
@@ -75,24 +104,30 @@ const SetCEFRReminder = ({ open, setOpen, newUser }) => {
               <CERFLevelSlider sliderValue={sliderValue} setSliderValue={setSliderValue} />
             </>
           )}
-          <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '1rem', borderTop: '1px solid #ddd', paddingTop: '1rem' }}>
-            {step > 1 && !isTeacher && (
-              <Button variant="secondary" onClick={() => setStep(step - 1)}>
-                <FormattedMessage id="Back" />
-              </Button>
-            )}
-            <div style={{ marginLeft: 'auto' }}>
-              {isTeacher || step === 3 ? (
-                <Button variant="primary" onClick={submitSettings}>
-                  <FormattedMessage id="Save" />
-                </Button>
-              ) : (
-                <Button variant="primary" onClick={() => setStep(step + 1)}>
-                  <FormattedMessage id="next" />
+          {step !== 1 && (
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                marginTop: '1rem',
+                borderTop: '1px solid #ddd',
+                paddingTop: '1rem',
+              }}
+            >
+              {step > 1 && (
+                <Button variant="secondary" onClick={() => setStep(step - 1)}>
+                  <FormattedMessage id="Back" />
                 </Button>
               )}
+              <div style={{ marginLeft: 'auto' }}>
+                {step === 3 && (
+                  <Button variant="primary" onClick={submitSettings}>
+                    <FormattedMessage id="Save" />
+                  </Button>
+                )}
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </Modal.Content>
     </Modal>
