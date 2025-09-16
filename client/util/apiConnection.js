7b51d07596552d12e8b852e9ae3ab2d768890dd9
Robert Pakkanen <rpakkane@gmail.com>
2025-09-16 12:55:18 +0300

    Remove debug log

--- client/util/apiConnection.js
+++ client/util/apiConnection.js
@@ -161,7 +161,6 @@ export const handleRequest = store => next => async action => {
         }
 
         if (cache) {
-          console.log('caching, ', cache, 'data:', res.data)
           window.localStorage.setItem(cache, JSON.stringify(res.data))
         }
         if (res.data?.new_achievements?.length > 0) {
