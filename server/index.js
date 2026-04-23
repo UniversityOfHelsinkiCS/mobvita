c7a9228bebcacd7b0a4ea6e58fc137d9a899db70
opturtio <olli-pekka.turtio@helsinki.fi>
2026-04-23 14:58:04 +0300

    update action.yml to build app before start and update server to serve Vite dist

--- server/index.js
+++ server/index.js
@@ -1,22 +1,28 @@
+const path = require('path')
 const express = require('express')
 const routes = require('@util/routes')
 const errorMiddleware = require('@middleware/errorMiddleware')
-
 const headers = require('@middleware/headerMiddleware')
 const swaggerUi = require('swagger-ui-express')
 const YAML = require('yamljs')
-
 const fileCheck = require('@middleware/fileCheckMiddleware')
 
 const swaggerDocument = YAML.load('./swagger.yaml')
-
 const app = express()
+
+const distPath = path.join(__dirname, '..', 'dist')
 
 app.use(express.json({ limit: '10mb' }))
 app.use(fileCheck)
 app.use(headers)
+
 app.use('/swag', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use(express.static(distPath))
 app.use(routes)
+
+app.get('*', (req, res) => {
+  return res.sendFile(path.join(distPath, 'index.html'))
+})
 
 app.use(errorMiddleware)
 
