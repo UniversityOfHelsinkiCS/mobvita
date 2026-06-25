8c45212e84008044543c170b7c1efacca414dd40
opturtio <olli-pekka.turtio@helsinki.fi>
2026-06-25 13:49:41 +0300

    Add localization to error message

--- client/components/EssayWritingView/CorrectionSuggestionPopper.js
+++ client/components/EssayWritingView/CorrectionSuggestionPopper.js
@@ -7,6 +7,7 @@ import {
   getWritingCorrectionWords,
   writingCorrectionHasChanges,
 } from 'Utilities/redux/writingCorrectionReducer'
+import { FormattedMessage } from 'react-intl'
 
 const CorrectionSuggestionPopper = ({
   correctionEntry,
@@ -32,7 +33,7 @@ const CorrectionSuggestionPopper = ({
         <Box className="essay-writing-correction-content">
           {hiddenFeatures && (
             <Box component="span" className="essay-writing-correction-error-tag">
-              Error
+              <FormattedMessage id="error" />
             </Box>
           )}
         </Box>
